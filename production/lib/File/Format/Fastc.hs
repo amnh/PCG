8a7b00680ccfa11ddbd764602d5a362805404ece
Alex Washburn <awashburn@amnh.org>
2015-10-14 15:34:43 -0400

    Added proper Fastc module export

---
+++ production/lib/File/Format/Fastc.hs
@@ -0,0 +1,11 @@
+{-# LANGUAGE FlexibleContexts #-}
+
+module File.Format.Fastc
+  ( CharacterSequence
+  , FastcParseResult
+  , FastcSequence(..)
+  , Identifier
+  , fastcStreamParser
+  ) where
+
+import File.Format.Fastc.Parser
