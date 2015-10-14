2f50b1be0c0f12a44246f7ac45bfd648120f622b
Alex Washburn <awashburn@amnh.org>
2015-10-14 14:17:01 -0400

    Added abstracted parser testing functions to be used in all parser test suites

---
+++ production/lib/Test/Custom.hs
@@ -0,0 +1,33 @@
+module Test.Custom
+  ( parseEquals
+  , parseFailure
+  , parseSuccess
+  ) where
+
+import Test.Tasty.HUnit
+import Text.Parsec
+
+parseSuccess :: Parsec String () a -> String -> Assertion
+parseSuccess parser str =
+  case result of
+    Left  x -> assertFailure $ show x
+    Right _ -> assert True
+  where
+    result = parse parser "" str
+
+parseFailure :: Parsec String () a -> String -> Assertion
+parseFailure parser str =
+  case result of
+    Right _ -> assertFailure $ "Should have failed to parse input: str"
+    Left  _ -> assert True
+  where
+    result = parse parser "" str
+
+parseEquals :: (Eq a, Show a) => Parsec String () a -> String -> a -> Assertion
+parseEquals parser str expected =
+  case result of
+    Left  x -> assertFailure $ show x
+    Right x -> assertEqual ("Parse result of :" ++ show str) expected x
+  where
+    result = parse parser "" str
+
