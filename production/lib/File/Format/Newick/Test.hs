bcb7922a3bed2148de51b34eb3ab2446a3d7aedd
Alex Washburn <awashburn@amnh.org>
2015-10-16 09:30:14 -0400

    More Newick tests

--- production/lib/File/Format/Newick/Test.hs
+++ production/lib/File/Format/Newick/Test.hs
@@ -16,11 +16,11 @@ import Text.Parsec                (parse,eof)
 testSuite :: TestTree
 testSuite = testGroup "Newick Format"
   [ testGroup "Newick Combinators"
-      [unquotedLabel',quotedLabel',newickBranchLength']
+      [unquotedLabel',quotedLabel',newickBranchLength',newickLeaf',descendantList']
   , testGroup "Newick Parser" 
-      [newickLeaf',descendantList']
+      [newickStandardDefinition']
   , testGroup "Extended Newick Parser"
-      []
+      [newickExtendedDefinition']
   , testGroup "Forest Newick Parser"
       []
   , testGroup "Newick Converter"
@@ -118,135 +118,32 @@ descendantList' = testGroup "descendantListDefinition" [valid,invalid]
       , "(wow such very much)" -- No commas
       ]
 
-{-
-
-
-
-identifier' :: TestTree
-identifier' = testGroup "identifier" [invariant, valid, invalid]
+newickStandardDefinition' :: TestTree
+newickStandardDefinition' = testGroup "newickStandardDefinition" [valid,invalid]
   where
-    valid         = testGroup "Valid taxon labels"   $ success <$>   validTaxonLabels
-    invalid       = testGroup "Invalid taxon labels" $ failure <$> invalidTaxonLabels
-    success str   = testCase (show str) $ parseEquals  (identifier <* eof) str str
-    failure str   = testCase (show str) $ parseFailure (identifier <* eof) str
-    invalidTaxonLabels =
-      [ x ++ [s] ++ y |  e    <- validTaxonLabels
-                      ,  s    <- "$ \t\r\n"
-                      ,  i    <- [length e `div` 2]
-                      , (x,y) <- [i `splitAt` e] 
+    valid         = testGroup "Valid Newick trees"   $ success <$> validTrees
+    invalid       = testGroup "Invalid Newick trees" $ failure <$> invalidTrees
+    success str   = testCase (show str) $ parseSuccess (newickStandardDefinition <* eof) str
+    failure str   = testCase (show str) $ parseFailure (newickStandardDefinition <* eof) str
+    validTrees =
+      [ "(left,right)root:1;"
+      , "(((1,2),3),(4,5));"
       ]
-    invariant = testProperty "fastaLabel invariant" f
-      where 
-        f x = null str || parse identifier "" str == Right res
-          where
-            str = takeWhile validIdentifierChar x
-            res = headOrEmpty $ words str
+    invalidTrees =
+      [ "(left,right)root:1"  -- Missing ending semicolon
+      , "(((1,2),3),(4,5):);" -- Missing length after colon
+      ]
 
-validTaxonLabels :: [String]
-validTaxonLabels = 
-  [ "Peripatidae"
-  , "Colossendeis"
-  , "Ammotheidae"
-  , "Buthidae"
-  , "Mygalomorphae"
-  ]
-
-commentBody' :: TestTree
-commentBody' = testGroup "commentBody" [generalComment, prependedDollarSign, validComments]
+newickExtendedDefinition' :: TestTree
+newickExtendedDefinition' = testGroup "newickExtendedDefinition" [valid,invalid]
   where
-    generalComment :: TestTree
-    generalComment = testProperty "General comment structure" f
-      where
-        f x = hasLeadingDollarSign 
-           || null res
-           || parse commentBody "" x == Right res
-          where
-            res = unwords . words $ takeWhile (not.(`elem`"\n\r")) x
-            hasLeadingDollarSign = let y = dropWhile isSpace x
-                                   in  headMay y == Just '$'
-    prependedDollarSign :: TestTree
-    prependedDollarSign = testProperty "Comments defined with leading dollar sign" $ prepended '$'
-      where
-        prepended :: Char -> String -> Bool
-        prepended c x = null x || null line'
-                     || ( parse commentBody "" (            c   : line) == Right line'
-                       && parse commentBody "" (      ' ' : c   : line) == Right line'
-                       && parse commentBody "" (      c   : ' ' : line) == Right line'
-                       && parse commentBody "" (' ' : c   : ' ' : line) == Right line'
-                        )
-          where
-            line  = takeWhile (not . (`elem`"\n\r")) x
-            line' = unwords $ words line
-    validComments = testGroup "Valid comments" $ success <$> validCommentBodies
-      where 
-        success str = testCase (show str) $ parseEquals (commentBody <* eof) str "A species of animal"
-
-validCommentBodies :: [String]
-validCommentBodies =
-  [ "$A species of animal"
-  , " $A species of animal"
-  , "$ A species of animal"
-  , " $ A species of animal"
-  , " A species of animal"
-  ]
-
-identifierLine' :: TestTree
-identifierLine' = testGroup "fastaLabelLine" [validWithoutComments, validWithComments]
-  where
-    validWithoutComments = testGroup "Valid taxon label lines without comemnts" $ success <$> validTaxonCommentlessLines
-    validWithComments    = testGroup "Valid taxon label lines with comments"    $ success <$> validTaxonCommentLines
-    success (res,str)    = testCase (show str) $ parseEquals (identifierLine <* eof) str res
-
-validTaxonCommentLines     :: [(String, String)]
-validTaxonCommentLines     = zip validTaxonLabels validTaxonCommentedLabels 
-validTaxonCommentlessLines :: [(String, String)]
-validTaxonCommentlessLines = zip  validTaxonLabels (inlineLabel <$> validTaxonLabels)
-validTaxonCommentedLabels  :: [String]
-validTaxonCommentedLabels  = inlineLabel <$> zipWith (++) validTaxonLabels validCommentBodies
-inlineLabel :: String -> String
-inlineLabel x = concat ["> ", x, "\n"]
-
-fastaSequence' :: TestTree
-fastaSequence' = testGroup "fastaSequence" [valid,nonDNAValid]
-  where
-    valid             = testGroup "Valid DNA sequences"     $ success <$> validSequences
-    nonDNAValid       = testGroup "Valid non-DNA sequences" $ success <$> nonDNASequences
-    success (res,str) = testCase (show str) $ parseEquals fastaSequence str res
-    nonDNASequences   = [ ("-.?"                 , "-.?\n"                 ) -- Gap / Missing
-                        , ("#"                   , "#\n"                   ) -- Sequence Partition 
-                        , ("RYSWKMBDHVN"         , "RYSWKMBDHVN\n"         ) -- IUPAC Ambiguity Codes
-                        , ("ACDEFGHIKLMNPQRSTVWY", "ACDEFGHIKLMNPQRSTVWY\n") -- AminoAcids
-                        ]
--- add X as ambiguity for AminoAcids
-validSequences :: [(String,String)]
-validSequences =
-  [ ("-GATACA-"            , "-GATACA-\n"            )
-  , ("-GATACA-"            , "- G ATA CA- \n"        )
-  , ("-GATACA-"            , "-GAT\nACA-\n"          )
-  , ("-GATACA-"            , " -G A\nT\n AC A- \n"   )
-  , ("-GATACA-"            , "-GA\n\nT\n \nACA-\n"   )
-  ]
-
-fastaTaxonSequenceDefinition' :: TestTree
-fastaTaxonSequenceDefinition' = testGroup "fastaTaxonSequenceDefinition" [valid]
-  where
-    valid             = testGroup "Valid sequences" $ success <$> validTaxonSequences
-    success (res,str) = testCase (show str) $ parseEquals fastaTaxonSequenceDefinition str res
-
-validTaxonLines     :: [(String,String)]
-validTaxonLines     = validTaxonCommentLines ++ validTaxonCommentlessLines
-validTaxonSequences :: [(FastaSequence,String)]
-validTaxonSequences = zipWith f validTaxonLines validSequences
-  where
-    f (x,str) (y,seq')  = (FastaSequence x y, concat [str,"\n",seq'])
-
-fastaStreamParser' :: TestTree
-fastaStreamParser' = testGroup "fastaStreamParser" [testGroup "Valid stream" [validStream]]
-  where
-    validStream = testCase "Concatenateed fasta stream" $ parseEquals fastaStreamParser str res
-    (res,str)   = second concat $ unzip validTaxonSequences
-
-headOrEmpty :: [[a]] -> [a]
-headOrEmpty = fromMaybe [] . headMay
-
--}
+    valid         = testGroup "Valid Newick trees"   $ success <$> validTrees
+    invalid       = testGroup "Invalid Newick trees" $ failure <$> invalidTrees
+    success str   = testCase (show str) $ parseSuccess (newickExtendedDefinition <* eof) str
+    failure str   = testCase (show str) $ parseFailure (newickExtendedDefinition <* eof) str
+    validTrees =
+      [ "(((1,2),X),((3,4)X,5));" -- Acyclical node merge
+      ]
+    invalidTrees =
+      [ "(((1,2),X)Y,((3,Y)X,4));" -- Cyclic node merge, non-sensical
+      ]
