254e2efdeaa820e57b0cc7146acebac7473f7d3c
Alex Washburn <awashburn@amnh.org>
2015-12-02 17:41:16 -0500

    Used either library for proper either combinators

--- production/lib/Data/Either/Custom.hs
+++ production/lib/Data/Either/Custom.hs
@@ -1,15 +1,21 @@
 module Data.Either.Custom where
 
-isLeft, isRight :: Either a b -> Bool
-isLeft (Left _) = True
-isLeft _        = False
-isRight = not . isLeft
+import Control.Monad.Trans.Either
+import Data.Either                (partitionEithers)
+import Data.Semigroup
 
-leftMay  :: Either a b  -> Maybe a
-leftMay (Left x) = Just x
-leftMay _        = Nothing
+-- | Allows for the coalescence of Left values that form a Semigroup.
+--   Overrides short-circuiting behaviour of the standard monadic `sequence` definition.
+--   The validation context will evaluate the entire [Either e a] returning a Right value
+--   if and only if only Right values were present in the list of Eithers. If any Left
+--   values were present in the list of Eithers then a Left value is returned consisting
+--   of the an order preservign fold using the semigroup operation `(<>)`.
+eitherValidation :: Semigroup e => [Either e a] -> Either e [a]
+eitherValidation xs =
+  case partitionEithers xs of
+    ([] , res) -> pure res
+    (err, _  ) -> Left $ foldl1 (<>) err
 
-rightMay :: Either a b  -> Maybe b
-rightMay (Right x) = Just x
-rightMay _         = Nothing
-
+-- |
+eitherTValidation :: (Monad m, Semigroup e) => [EitherT e m a] -> EitherT e m [a]
+eitherTValidation = EitherT . fmap eitherValidation . sequence . fmap runEitherT
