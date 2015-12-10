31e1b988e387efa64d28da51e0d3a3611d157979
Alex Washburn <awashburn@amnh.org>
2015-12-10 11:07:02 -0500

    Initial data-set from Ming for FASTC format

---
+++ production/test/TestSuite/GeneratedTests/Fastc.hs
@@ -0,0 +1,38 @@
+{-# LANGUAGE FlexibleContexts #-}
+
+module TestSuite.GeneratedTests.Fastac
+  ( testSuite
+  ) where
+
+import Data.Either.Combinators
+import Data.Map                          (toList)
+import File.Format.Fastc
+import Test.Tasty                        (TestTree,testGroup)
+import Test.Tasty.HUnit
+import TestSuite.GeneratedTests.Internal
+import Text.Megaparsec                   (parse)
+
+testSuite :: IO TestTree
+testSuite = testGroup "fastcStreamParser" <$> sequence [validFastaFiles, {-invalidFastaFiles-}]
+
+validFastaFiles :: IO TestTree
+validFastaFiles = validateFileContents <$> validContents
+  where
+    validContents          = getFileContentsInDirectory "test/data-sets/fastc/valid"
+    validateFileContents   = testGroup "Valid files" . fmap success . toList
+    success (path,content) = testCase (show path) 
+                           $ case result of
+                               Left x  -> assertFailure $ show x
+                               Right _ -> assert True
+      where
+       result = parse fastcStreamParser path content
+{-
+invalidFastaFiles :: IO TestTree
+invalidFastaFiles = validateFileContents <$> validContents
+  where
+    validContents          = getFileContentsInDirectory "test/data-sets/fasta/invalid"
+    validateFileContents   = testGroup "Invalid files" . fmap failure . toList
+    failure (path,content) = testCase (show path) . assert $ isLeft result
+      where
+        result = parse fastaStreamParser path content
+-}
