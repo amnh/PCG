db57e116ed9e6c6139e0ed6d930dde7d0a0e6918
ganewman42 <ganewman42@gmail.com>
2015-12-14 10:13:50 -0500

    Changes to coded sequence

---
+++ production/lib/Bio/Sequence/Coded/Class.hs
@@ -0,0 +1,8 @@
+{-# LANGUAGE MultiParameterTypeClass #-}
+
+module Bio.Sequence.Coded.Class where
+
+class Monoid s => CodedSequence s b where
+    numChars :: s -> Int
+    gapChar :: s
+    grabChar :: s -> Int -> b
