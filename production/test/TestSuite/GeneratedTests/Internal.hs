615fbd845a5fbbda9be563185b2300ff18eaec59
Alex Washburn <awashburn@amnh.org>
2015-12-16 12:00:24 -0500

    Added support for that build tool that is poorly named after a datastructure, improved test suite robustness

--- production/test/TestSuite/GeneratedTests/Internal.hs
+++ production/test/TestSuite/GeneratedTests/Internal.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DoAndIfThenElse #-}
 module TestSuite.GeneratedTests.Internal where
 
 import Control.Arrow    ((&&&))
@@ -7,8 +8,12 @@ import System.Directory
 -- | Gets all the given files and thier contents in the specified directory
 getFileContentsInDirectory :: FilePath -> IO (Map FilePath String)
 getFileContentsInDirectory path = do
-    files <- filter isFile <$> getDirectoryContents path
-    sequence . fromList $ (id &&& readFile) . withPath <$> files
+    exists <- doesDirectoryExist path
+    if not exists
+    then pure mempty
+    else (do
+      files  <- filter isFile <$> getDirectoryContents path
+      sequence . fromList $ (id &&& readFile) . withPath <$> files)
   where
     isFile = not . all (=='.')
     withPath file = if last path /= '/'
