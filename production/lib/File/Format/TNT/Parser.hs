52dfc8481465065afb0e93874581fc255f0fb3a1
Alex Washburn <awashburn@amnh.org>
2015-12-18 14:37:09 -0500

    TNT First steps

--- production/lib/File/Format/TNT/Parser.hs
+++ production/lib/File/Format/TNT/Parser.hs
@@ -1,1 +1,39 @@
+{-# LANGUAGE FlexibleContexts #-}
 module File.Format.TNT.Parser where
+
+import Data.Char              (isSpace)
+import Text.Megaparsec
+import Text.Megaparsec.Custom
+import Text.Megaparsec.Lexer  (integer)
+import Text.Megaparsec.Prim   (MonadParsec)
+
+type TaxonInfo     = (TaxonName, TaxonSequence) 
+type TaxonName     = String
+type TaxonSequence = String
+
+xreadBlock :: MonadParsec s m Char => m [TaxonInfo]
+xreadBlock = do
+    _        <- symbol $ string' "xread"
+    _        <- optional $ try $ symbol $ comment (string "'") (string "'")
+    numOne   <- symbol $ integer
+    numTwo   <- symbol $ integer
+    taxaSeqs <- symbol $ some taxonSequence
+    _        <- symbol $ char ';'
+    pure taxaSeqs
+
+taxonSequence :: MonadParsec s m Char => m TaxonInfo
+taxonSequence = do
+    name <- symbol $ taxonName
+    seq' <- taxonSeq
+    pure (name, seq')
+  where
+    taxonName     = some validNameChar
+    taxonSeq      = validSeqChar `someTill` endOfLine
+    validNameChar = satisfy (not . isSpace)
+    validSeqChar  = oneOf $ ['0'..'9'] ++ ['A'..'Z'] ++ ['a'..'z'] ++ "-?"
+
+symbol :: MonadParsec s m Char => m a -> m a
+symbol c = c <* whitespace
+
+whitespace :: MonadParsec s m Char => m ()
+whitespace = space
