fb8667d006096cc7d392b75890d4540bf6ae86cf
Washburn <github@recursion.ninja>
2015-12-21 15:05:36 -0500

    Initial XREAD comments

--- production/lib/File/Format/TNT/Parser.hs
+++ production/lib/File/Format/TNT/Parser.hs
@@ -6,18 +6,20 @@ module File.Format.TNT.Parser where
   - Good documentation
   -}
 
-import           Data.Bifunctor         (second)
-import           Data.Char              (isSpace)
-import           Data.DList             (DList,append)
-import qualified Data.DList as DL       (toList,fromList)
-import           Data.List              (intersperse)
-import           Data.Map.Strict        (Map,insertWith)
-import qualified Data.Map.Strict as M   (toList)
-import           Data.Maybe             (catMaybes)
+import           Data.Bifunctor           (second)
+import           Data.Char                (isSpace)
+import           Data.DList               (DList,append)
+import qualified Data.DList         as DL (toList,fromList)
+import           Data.List                (intersperse)
+import           Data.List.NonEmpty       (NonEmpty)
+import qualified Data.List.NonEmpty as NE (filter,fromList,length)
+import           Data.Map.Strict          (Map,insertWith)
+import qualified Data.Map.Strict    as M  (toList)
+import           Data.Maybe               (catMaybes)
 import           Text.Megaparsec
 import           Text.Megaparsec.Custom
-import           Text.Megaparsec.Lexer  (integer,number,signed)
-import           Text.Megaparsec.Prim   (MonadParsec)
+import           Text.Megaparsec.Lexer    (integer,number,signed)
+import           Text.Megaparsec.Prim     (MonadParsec)
 
 -- | The sequence information for a taxon within the TNT file's XREAD command.
 -- Contains the 'TaxonName' and the naive 'TaxonSequence' 
@@ -29,19 +31,21 @@ type TaxonName     = String
 -- | The naive sequence of a taxon in a TNT files' XREAD command.
 type TaxonSequence = String
 
-xreadCommand :: MonadParsec s m Char => m [TaxonInfo]
+-- | Parses an XREAD command. Correctly validates for taxa count
+-- and character sequence length. Produces one or more taxa sequences.
+xreadCommand :: MonadParsec s m Char => m (NonEmpty TaxonInfo)
 xreadCommand = xreadValidation =<< xreadDefinition
   where
-    xreadDefinition :: MonadParsec s m Char => m (Int, Int, [TaxonInfo])
+    xreadDefinition :: MonadParsec s m Char => m (Int, Int, NonEmpty TaxonInfo)
     xreadDefinition = uncurry (,,) <$> xreadPreamble <*> xreadSequences <* symbol (char ';')
 
-    xreadValidation :: MonadParsec s m Char => (Int, Int, [TaxonInfo]) -> m [TaxonInfo]
+    xreadValidation :: MonadParsec s m Char => (Int, Int, NonEmpty TaxonInfo) -> m (NonEmpty TaxonInfo)
     xreadValidation (taxaCount, charCount, taxaSeqs)
       | null errors = pure taxaSeqs
       | otherwise   = fails errors
       where
         errors = catMaybes $ [taxaCountError, charCountError]
-        taxaCountError = let taxaLength = length taxaSeqs
+        taxaCountError = let taxaLength = NE.length taxaSeqs
                          in if taxaCount == taxaLength
                             then Nothing
                             else Just $ concat
@@ -51,7 +55,7 @@ xreadCommand = xreadValidation =<< xreadDefinition
                               , show $ length taxaSeqs
                               , ")"
                               ]
-        charCountError = case filter ((/= charCount) . snd) . fmap (second length) $ taxaSeqs of
+        charCountError = case NE.filter ((/= charCount) . snd) . fmap (second length) $ taxaSeqs of
                            [] -> Nothing
                            xs -> Just $ concat
                               [ "The number of characters specified ("
@@ -60,27 +64,53 @@ xreadCommand = xreadValidation =<< xreadDefinition
                               , unlines $ prettyPrint <$> xs
                               ]                            
         prettyPrint (name, count) = concat ["\t",show name," found (",show count,") characters"]
-                          
+
+-- | Consumes everything in the XREAD command prior to the taxa sequences.
+-- Produces the expected taxa count and the length of the character sequences.
 xreadPreamble :: MonadParsec s m Char => m (Int, Int)
 xreadPreamble = xreadHeader *> ((,) <$> xreadTaxaCount <*> xreadCharCount)
 
+-- | The superflous information of an XREAD command.
+-- Consumes the XREAD string identifier and zero or more comments
+-- preceeding the taxa count and character cound parameters
 xreadHeader :: MonadParsec s m Char => m ()
 xreadHeader =  symbol (string' "xread")
-            *> optional (try simpleComment)
+            *> many simpleComment
             *> pure ()
   where
     simpleComment = delimiter *> anythingTill delimiter <* symbol delimiter
       where
         delimiter = char '\''
 
+-- | The number of taxa present in the XREAD command.
+-- __Naturally__ this number must be a positive integer.
 xreadTaxaCount :: MonadParsec s m Char => m Int
 xreadTaxaCount = symbol $ flexiblePositiveInt "taxa count" 
 
+-- | The number of characters in a taxon sequence for this XREAD command.
+-- __Naturally__ this number must be a positive integer.
 xreadCharCount :: MonadParsec s m Char => m Int
-xreadCharCount = symbol $ flexiblePositiveInt "char count" 
-                  
-xreadSequences :: MonadParsec s m Char => m [TaxonInfo]
-xreadSequences = deinterleaveTaxa <$> symbol (some taxonSequence)
+xreadCharCount = symbol $ flexiblePositiveInt "char count"
+
+-- | Reads one or more taxon sequences.
+-- Performs deinterleaving of identically named taxon sequences. 
+-- ==== __Examples__
+--
+-- Basic usage:
+--
+-- >>> parse xreadSequences "" "taxonOne GATACA\ntaxonTwo GGAATT"
+-- Right [ ("taxonOne", "GATACA")
+--       , ("taxonTwo", "GGAATT")
+--       ]
+--
+-- Interleaved usage:
+--
+-- >>> parse xreadSequences "" "taxonOne GATACA\ntaxonTwo GGAATT\ntaxonOne ACATAG\ntaxonTwo CCGATC\n"
+-- Right [ ("taxonOne", "GATACAACATAG")
+--       , ("taxonTwo", "GGAATTCCGATC")
+--       ]
+xreadSequences :: MonadParsec s m Char => m (NonEmpty TaxonInfo)
+xreadSequences = NE.fromList . deinterleaveTaxa <$> symbol (some taxonSequence)
   where
     deinterleaveTaxa :: [TaxonInfo] -> [TaxonInfo]
     deinterleaveTaxa = M.toList . fmap DL.toList . foldr f mempty 
@@ -88,18 +118,44 @@ xreadSequences = deinterleaveTaxa <$> symbol (some taxonSequence)
         f :: TaxonInfo -> Map TaxonName (DList Char) -> Map TaxonName (DList Char)
         f (taxonName, taxonSequence) = insertWith append taxonName (DL.fromList taxonSequence)
 
+-- | Parses a taxon name and sequence of characters for a given character.
+-- Character values can be one of 64 states ordered @[0..9,A..Z,a..z]@ and also the Chars @\'-\'@ & @\'?\'@.
+-- Taxon name cannot contain spaces or the @\';\'@ character.
 taxonSequence :: MonadParsec s m Char => m TaxonInfo
 taxonSequence = (,) <$> (symbol taxonName) <*> taxonSeq
   where
     taxonName     = some validNameChar
-    taxonSeq      = validSeqChar `someTill` terminal
-    terminal      = whitespaceInline *> endOfLine
-    validNameChar = satisfy (\x -> (not . isSpace) x && x /= ';')
+    taxonSeq      = validSeqChar `someTill` endOfLine
+    validNameChar = satisfy (\x -> (not . isSpace) x && x /= ';') <* whitespaceInline
     validSeqChar  = oneOf $ ['0'..'9'] ++ ['A'..'Z'] ++ ['a'..'z'] ++ "-?"
 
 -- | Parses an positive integer from a variety of representations.
 -- Parses both signed integral values and signed floating values
 -- if the value is positive and an integer.
+--
+-- @flexiblePositiveInt labeling@ uses the @labeling@ parameter to
+-- improve ParseError generation.
+--
+-- ==== __Examples__
+--
+-- Basic usage:
+--
+-- >>> parse (flexiblePositiveInt "errorCount") "" "1\n"
+-- Right 1
+--
+-- >>> parse (flexiblePositiveInt "errorCount") "" "0\n"
+-- Left 1:2:
+-- expecting rest of number
+-- The errorCount value (0) is not a positive number
+--
+-- >>> parse (flexiblePositiveInt "errorCount") "" "42.0\n"
+-- Right 42
+--
+-- >>> parse (flexiblePositiveInt "errorCount") "" "0.1337\n"
+-- Left 1:7:
+-- expecting 'E', 'e', or rest of number
+-- The errorCount value (0.1337) is a real value, not an integral value
+-- The errorCount value (0.1337) is not a positive integer
 flexiblePositiveInt :: MonadParsec s m Char => String -> m Int
 flexiblePositiveInt labeling = either coerceIntegral coerceFloating
                              =<< signed whitespace number <?> ("positive integer for " ++ labeling)
