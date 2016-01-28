24bdfeca22480ed80502a427b0a21042c4986271
ganewman42 <ganewman42@gmail.com>
2016-01-28 12:03:23 -0500

    Merge changes to encoded node

--- prototype/Data/Tree/Node/Encoded.hs
+++ prototype/Data/Tree/Node/Encoded.hs
@@ -5,7 +5,8 @@ module Data.Tree.Node.Encoded where
 import Data.Bits
 import Data.Int
 import Data.Vector
+import Data.Sequence.Encoded
 
 class EncodedNode a b | a -> b where
-  encoded :: a -> Maybe (Vector (Vector b))
-  setEncoded :: a -> Maybe (Vector (Vector b)) -> a
+  encoded :: a -> EncodedSeq
+  setEncoded :: a -> EncodedSeq -> a
