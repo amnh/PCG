fb9c0ba17d6a1749fb962b61a27ac77b6bd8cf2d
Alex Washburn <awashburn@amnh.org>
2016-03-21 17:45:41 -0400

    Rectification progress...

--- production/app/PCG/Command/Types/Read/Evaluate.hs
+++ production/app/PCG/Command/Types/Read/Evaluate.hs
@@ -4,28 +4,24 @@ module PCG.Command.Types.Read.Evaluate
   ( evaluate
   ) where
 
-import           Bio.Phylogeny.Graph
-import           Bio.Sequence.Parsed
-import           Control.Arrow              ((&&&))
-import           Control.Monad              (liftM2,when)
+import           Bio.Phylogeny.Graph.Parsed
+import           Bio.Metadata.Class
+import           Bio.Sequence.Parsed.Class
+import           Control.Monad              (when)
 import           Control.Monad.IO.Class
 import           Control.Monad.Trans.Either
 import           Control.Evaluation
-import           Data.Bifunctor             (first)
-import           Data.Char                  (toLower)
-import           Data.Either                (partitionEithers)
-import           Data.Either.Combinators    (isRight, rightToMaybe)
+import           Data.Bifunctor             (bimap,first)
 import           Data.Either.Custom
 import           Data.Foldable
-import           Data.Map                   (Map)
-import           Data.Maybe                 (fromJust,isNothing)
-import           Data.Hashable
-import           Data.HashMap.Strict        (HashMap)
-import qualified Data.HashMap.Strict  as HM (fromList)
-import           Data.Text.Lazy             (Text)
+--import           Data.Hashable
+--import           Data.HashMap.Strict        (HashMap)
+--import qualified Data.HashMap.Strict  as HM (fromList)
 import           File.Format.Fasta
 import           File.Format.Fastc   hiding (Identifier)
 import           File.Format.Newick
+import           File.Format.Nexus          (nexusStreamParser)
+import           File.Format.TNT
 import           File.Format.TransitionCostMatrix
 import           File.Format.VertexEdgeRoot
 -- import File.Format.Conversion.ToInternal
@@ -37,71 +33,118 @@ import           Prelude             hiding (lookup)
 import           Text.Megaparsec
 
 evaluate :: Command -> SearchState -> SearchState
-{-
+{--}
 evaluate (READ fileSpecs) old = do
     when (null fileSpecs) $ fail "No files specified in 'read()' command"
     result <- liftIO . runEitherT . eitherTValidation $ parseSpecifiedFile <$> fileSpecs
     case result of
       Left err -> fail $ show err   -- Report structural errors here.
-      Right xs -> foldl (<>) old xs -- Here we validate more
--}
+      Right xs -> mempty -- foldl (<>) old xs -- Here we validate more
+{--}
 evaluate _ _ = fail "Invalid READ command binding"
-{-
+{--}
 parseSpecifiedFile  :: FileSpecification -> EitherT ReadError IO [FracturedParseResult]
 parseSpecifiedFile      (AnnotatedFile      _    ) = fail "Annotated file specification is not implemented"
 parseSpecifiedFile      (ChromosomeFile     _    ) = fail "Chromosome file specification is not implemented"
 parseSpecifiedFile      (GenomeFile         _    ) = fail "Genome file specification is not implemented"
--- Currently ignoring TCM
-parseSpecifiedFile      (PrealignedFile     x _  ) = parseSpecifiedFile x
-
--- Currently ignoring TCM
-parseSpecifiedFile spec@(CustomAlphabetFile{}    ) = 
-  parseSpecifiedFileSimple  fastcStreamParser (setTaxaSeqs . customToHashMap) spec
-parseSpecifiedFile spec@(AminoAcidFile      _    ) =
-  parseSpecifiedFileSimple (fastaStreamConverter AminoAcid =<< fastaStreamParser) (setTaxaSeqs . fmap mapToHashMap) spec
-parseSpecifiedFile spec@(NucleotideFile     _    ) =
-  parseSpecifiedFileSimple (fastaStreamConverter DNA       =<< fastaStreamParser) (setTaxaSeqs . fmap mapToHashMap) spec
+parseSpecifiedFile spec@(AminoAcidFile      _    ) = fastaAminoAcid spec
+parseSpecifiedFile spec@(NucleotideFile     _    ) = fastaDNA       spec
+parseSpecifiedFile spec@(CustomAlphabetFile{}    ) = parseCustomAlphabet spec
+parseSpecifiedFile      (PrealignedFile     x tcmRef) = do
+    tcm <- getSpecifiedTcm tcmRef
+    res <- parseSpecifiedFile x
+    case tcm of
+      Nothing             -> pure res
+      Just (path,content) -> do
+        tcmMat <- hoistEither . first unparsable $ parse tcmStreamParser path content
+        traverse (setTcm tcmMat) res 
+  where
+    setTcm :: TCM -> FracturedParseResult -> EitherT ReadError IO FracturedParseResult
+    setTcm t fpr = case relatedTcm fpr of
+                     Nothing -> pure $ fpr { relatedTcm = Just t }
+                     Just _  -> fail "Multiple TCM files defined in prealigned file specification"
 parseSpecifiedFile spec@(UnspecifiedFile    _    ) =
-  getSpecifiedContent spec >>= fmap (foldl (<>) mempty) . eitherTValidation . fmap progressiveParse . dataFiles
+  getSpecifiedContent spec >>= eitherTValidation . fmap (progressiveParse . fst) . dataFiles
 
-setTaxaSeqs :: HashMap Identifier ParsedSequences -> SearchState
-setTaxaSeqs x = pure $ Graph [mempty { parsedSeqs = x }]
+fastaDNA :: FileSpecification -> EitherT ReadError IO [FracturedParseResult]
+fastaDNA spec = getSpecifiedContent spec >>= (hoistEither . parseSpecifiedContent)
+  where
+    parseSpecifiedContent :: FileSpecificationContent -> Either ReadError [FracturedParseResult]
+    parseSpecifiedContent = eitherValidation . fmap parse' . dataFiles
+    parse' :: FileResult -> Either ReadError FracturedParseResult
+    parse' (path,content) = toFractured Nothing path <$> parseResult
+      where
+        parseResult = first unparsable $ parse combinator path content
+        combinator  = (\x -> fastaStreamConverter DNA x <|> fastaStreamConverter RNA x) =<< fastaStreamParser
 
-mapToHashMap :: (Eq k, Hashable k) => Map k v -> HashMap k v
-mapToHashMap = fromList . M.toList
+-- TODO: abstract these two (three) v^
+fastaAminoAcid :: FileSpecification -> EitherT ReadError IO [FracturedParseResult]
+fastaAminoAcid spec = getSpecifiedContent spec >>= (hoistEither . parseSpecifiedContent)
+  where
+    parseSpecifiedContent :: FileSpecificationContent -> Either ReadError [FracturedParseResult]
+    parseSpecifiedContent = eitherValidation . fmap parse' . dataFiles
+    parse' :: FileResult -> Either ReadError FracturedParseResult
+    parse' (path,content) = toFractured Nothing path <$> parseResult
+      where
+        parseResult = first unparsable $ parse combinator path content
+        combinator  = fastaStreamConverter AminoAcid =<< fastaStreamParser
 
-customToHashMap :: [FastcParseResult] -> HashMap Identifier CharacterSequence
-customToHashMap = fromList . concatMap (fmap (fastcLabel &&& fastcSymbols) . toList)
+parseCustomAlphabet :: FileSpecification -> EitherT ReadError IO [FracturedParseResult]
+parseCustomAlphabet spec = getSpecifiedContent spec >>= (hoistEither . parseSpecifiedContent)
+  where
+    parseSpecifiedContent :: FileSpecificationContent -> Either ReadError [FracturedParseResult]
+    parseSpecifiedContent specContent = do
+        tcmMay <- case tcmFile specContent of
+                    Nothing             -> pure Nothing
+                    Just (path,content) -> bimap unparsable Just $ parse tcmStreamParser path content
+        eitherValidation . fmap (parse' tcmMay) $ dataFiles specContent
+    parse' m (path, content) = toFractured m path <$> parseResult
+      where
+        parseResult = first unparsable $ parse fastcStreamParser path content
 
+
+
+--setTaxaSeqs :: HashMap Identifier ParsedSequences -> SearchState
+--setTaxaSeqs x = pure $ Graph [mempty { parsedSeqs = x }]
+
+--mapToHashMap :: (Eq k, Hashable k) => Map k v -> HashMap k v
+--mapToHashMap = fromList . M.toList
+
+--customToHashMap :: [FastcParseResult] -> HashMap Identifier CharacterSequence
+--customToHashMap = fromList . concatMap (fmap (fastcLabel &&& fastcSymbols) . toList)
+{-
 parseSpecifiedFileSimple :: Parsec Text a -> (a -> FracturedParseResult-) -> FileSpecification -> EitherT ReadError IO SearchState
 parseSpecifiedFileSimple comb toState spec = getSpecifiedContent spec >>= (hoistEither . parseSpecifiedContent)
   where
     parseSpecifiedContent :: FileSpecificationContent -> Either ReadError SearchState
     parseSpecifiedContent = fmap toState . eitherValidation . fmap (first unparsable . parse') . dataFiles
     parse' (path,content) = parse comb path content
-
-progressiveParse :: FileResult -> EitherT ReadError IO SearchState
-progressiveParse (filePath, fileContent) =
-  case snd . partitionEithers $ parseTryOrderForSequences <*> [fileContent] of
-    parsed:_ -> pure . pure $ Graph [mempty { parsedSeqs = mapsToHashMap [parsed] }]
-    []       ->
---      case parse nexusStreamParser filePath fileContent of
---        Right _ -> pure mempty
---        Left  _ ->
-          case parse newickStreamParser filePath fileContent of
-            Right _ -> pure mempty
-            Left  _ ->
-              case parse tcmStreamParser  filePath fileContent of
-                Right _ -> pure mempty
-                Left  _ ->
-                  case parse verStreamParser filePath fileContent of
-                    Right _ -> pure mempty
-                    Left  _ -> fail $ "Could not determine the file type of '" ++ filePath ++ "'. Try annotating the expected file data in the 'read' for more explicit error message on file parsing failures."
-  where
-    parseTryOrderForSequences :: [FileContent -> Either ParseError TaxonSequenceMap]
-    parseTryOrderForSequences =
-      [ parse (fastaStreamConverter DNA       =<< fastaStreamParser) filePath
-      , parse (fastaStreamConverter RNA       =<< fastaStreamParser) filePath
-      , parse (fastaStreamConverter AminoAcid =<< fastaStreamParser) filePath
-      ]
 -}
+
+progressiveParse :: FilePath -> EitherT ReadError IO FracturedParseResult
+progressiveParse inputPath = do
+    res0 <- liftIO . runEitherT . parseSpecifiedFile $ NucleotideFile [inputPath]
+    case res0 of
+      Right x -> pure $ head x
+      Left  _ -> do
+        res1 <- liftIO . runEitherT . parseSpecifiedFile $ AminoAcidFile [inputPath]
+        case res1 of
+          Right x -> pure $ head x
+          Left  _ -> do
+            (filePath, fileContent) <- head . dataFiles <$> getSpecifiedContent (UnspecifiedFile [inputPath])
+            case parse newickStreamParser filePath fileContent of
+              Right x -> pure $ toFractured Nothing filePath x
+              Left  _ ->
+                case parse verStreamParser filePath fileContent of
+                  Right x -> pure $ toFractured Nothing filePath x
+                  Left  _ ->
+                    case parse tntStreamParser filePath fileContent of
+                      Right x -> pure $ toFractured Nothing filePath x
+                      Left  _ ->
+                        case parse nexusStreamParser filePath fileContent of
+                          Right x -> pure $ toFractured Nothing filePath x
+                          Left  _ -> fail $ "Could not determine the file type of '" ++ filePath ++ "'. Try annotating the expected file data in the 'read' for more explicit error message on file parsing failures."
+
+toFractured :: (Metadata a, ParsedCharacters a, ParseGraph a) => Maybe TCM -> FilePath -> a -> FracturedParseResult
+toFractured tcmMat path = FPR <$> unifyCharacters  <*> unifyMetadata <*> unifyGraph <*> const tcmMat <*> const path
+{--}
