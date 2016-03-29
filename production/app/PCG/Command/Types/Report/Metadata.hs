e8a6a0d54cc12cc7f60a78d136525ef3f5dfb933
Alex Washburn <awashburn@amnh.org>
2016-03-29 16:24:14 -0400

    Cleaned up Metadata printing

--- production/app/PCG/Command/Types/Report/Metadata.hs
+++ production/app/PCG/Command/Types/Report/Metadata.hs
@@ -17,6 +17,7 @@ module PCG.Command.Types.Report.Metadata where
 import Bio.Phylogeny.Graph
 import Bio.Phylogeny.PhyloCharacter
 import Data.Foldable
+import Data.List   (intercalate)
 import Data.Vector (fromList, ifoldr)
 
 --import Debug.Trace
@@ -39,14 +40,14 @@ metadataCsvOutput (Graph dags) = ifoldr oneCSV header (fromList dags)
                 myMeta = characters inDAG
 
 fetchInfo :: Show s => PhyloCharacter s -> String
-fetchInfo inChar =
-  case inChar of
-    (DNA         n a _ s alph _     i w) -> "DNA" ++ ", " ++ n ++ ", " ++ show a ++ ",, " ++ foldInfo s ++ ", " ++ foldInfo alph ++ ", " ++ show i ++ ", " ++ show w
-    (RNA         n a _ s alph _     i w) -> "RNA" ++ ", " ++ n ++ ", " ++ show a ++ ",, " ++ foldInfo s ++ ", " ++ foldInfo alph ++ ", " ++ show i ++ ", " ++ show w
-    (Qualitative n _ a s alph _ add i w) -> "Qualitative" ++ ", " ++ n ++ ", " ++ show a ++ ", " ++ show add ++ ", " ++ foldInfo s ++ ", " ++ foldInfo alph ++ ", " ++ show i ++ ", " ++ show w
-    (Continous   n i _ alph           w) -> "Continuous" ++ ", " ++ n ++ ",,,, " ++ foldInfo alph ++ ", " ++ show i ++ ", " ++ show w
-    (Custom      n a _ alph s _ i add w) -> "Custom" ++ ", " ++ n ++ ", " ++ show a ++ ", " ++ show add ++ ", " ++ foldInfo s ++ ", " ++ foldInfo alph ++ ", " ++ show i ++ ", " ++ show w
-    (AminoAcid   n a _ alph s _ i     w) -> "Amino Acid" ++ ", " ++ n ++ ", " ++ show a ++ ",, " ++ foldInfo s ++ ", " ++ foldInfo alph ++ ", " ++ show i ++ ", " ++ show w
+fetchInfo c = intercalate ", " $
+  case c of
+    DNA         {} -> ["DNA"        , name c, show $ aligned c,                "", foldInfo $ stateNames c, foldInfo $ alphabet c, show $ ignored c, show $ weight c ]
+    RNA         {} -> ["RNA"        , name c, show $ aligned c,                "", foldInfo $ stateNames c, foldInfo $ alphabet c, show $ ignored c, show $ weight c ]
+    Qualitative {} -> ["Qualitative", name c, show $ aligned c, show $ additive c, foldInfo $ stateNames c, foldInfo $ alphabet c, show $ ignored c, show $ weight c ]
+    Continous   {} -> ["Continuous" , name c,               "",                "",                      "", foldInfo $ alphabet c, show $ ignored c, show $ weight c ]
+    Custom      {} -> ["Custom"     , name c, show $ aligned c, show $ additive c, foldInfo $ stateNames c, foldInfo $ alphabet c, show $ ignored c, show $ weight c ]
+    AminoAcid   {} -> ["Amino Acid" , name c, show $ aligned c,                "", foldInfo $ stateNames c, foldInfo $ alphabet c, show $ ignored c, show $ weight c ]
 
 foldInfo :: (Foldable t, Show a) => t a -> String  
 foldInfo = unwords . fmap show . toList
