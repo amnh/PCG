9b38ba24d497b999e4e343b70f9834874f9113f6
Alex Washburn <awashburn@amnh.org>
2016-03-31 12:04:50 -0400

    Made rectifcation errors more human readable.

--- production/app/PCG/Command/Types/Read/Unification/UnificationError.hs
+++ production/app/PCG/Command/Types/Read/Unification/UnificationError.hs
@@ -1,13 +1,13 @@
 module PCG.Command.Types.Read.Unification.UnificationError where
 
-import Data.List.NonEmpty
+import Data.Foldable
+import Data.List.NonEmpty (NonEmpty)
 import Data.Semigroup
 
 type TaxaName = String
 
 data UnificationError
    = UnificationError (NonEmpty UnificationErrorMessage)
-   deriving (Show)
 
 data UnificationErrorMessage
    = NonMatchingTaxa     [TaxaName] [TaxaName] 
@@ -15,6 +15,9 @@ data UnificationErrorMessage
    | ForestDuplicateTaxa (NonEmpty TaxaName) FilePath
    | ForestExtraTaxa     (NonEmpty TaxaName) FilePath
    | ForestMissingTaxa   (NonEmpty TaxaName) FilePath
+
+instance Show UnificationError where
+  show (UnificationError xs) = unlines $ show <$> toList xs
 
 instance Show UnificationErrorMessage where
   show (NonMatchingTaxa xs ys) =
@@ -32,24 +35,24 @@ instance Show UnificationErrorMessage where
   show (ForestDuplicateTaxa names path) =
     concat [ "The trees from file '"
            , path
-           , "' contain an multiple entries for the following taxa: "
-           , show $ toList names
-           ,"."
+           , "' contain an multiple entries for the following taxa: \n"
+           , listShow names
            ]    
   show (ForestExtraTaxa names path) =
     concat [ "A tree from file '"
            , path
-           , "' contains an entry for the following taxa not included in the data set(s): "
-           , show $ toList names
-           ,"."
+           , "' contains an entry for the following taxa not included in the data set(s): \n"
+           , listShow names
            ]
   show (ForestMissingTaxa names path) =
     concat [ "None of the trees from file '"
            , path
-           , "' contain an entry for the taxa: "
-           , show $ toList names
-           ,"."
+           , "' contain an entry for the taxa: \n"
+           , listShow names
            ]
+
+listShow :: (Foldable t, Show a) => t a -> String
+listShow = (\x -> "[ " <> x <> "]") . drop 2 . unlines . fmap ((", " <>) . show) . toList
 
 instance Semigroup UnificationError where
     (UnificationError messages1) <> (UnificationError messages2) = UnificationError (messages1 <> messages2)
