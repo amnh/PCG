c2223752099dc25857f31dbf5f26ed72d0b53173
Alex Washburn <awashburn@amnh.org>
2016-04-26 15:42:12 -0400

    Added MonoFoldable instance to BitMatrix

--- production/lib/Data/BitMatrix.hs
+++ production/lib/Data/BitMatrix.hs
@@ -3,7 +3,8 @@ module Data.BitMatrix where
 
 import Data.Bifunctor
 import Data.Bits
-import Data.BitVector
+import Data.BitVector hiding (foldr)
+import Data.Foldable
 import Data.Monoid
 import Data.MonoTraversable
 
@@ -27,3 +28,37 @@ rows bm@(BitMatrix n bv) = (bv @@) <$> slices
         
 instance MonoFunctor BitMatrix where
   omap f bm = BitMatrix (numCols bm) . mconcat $ f <$> rows bm
+
+instance MonoFoldable BitMatrix where
+
+  -- | Map each element of a monomorphic container to a 'Monoid'
+  -- and combine the results.
+  ofoldMap f = ofoldr (mappend . f) mempty
+  {-# INLINE ofoldMap #-}
+
+  -- | Right-associative fold of a monomorphic container.
+  ofoldr f e = foldr f e . rows
+  {-# INLINE ofoldr #-}
+
+  -- | Strict left-associative fold of a monomorphic container.
+  ofoldl' f e = foldl' f e . rows
+  {-# INLINE ofoldl' #-}
+
+  -- | Right-associative fold of a monomorphic container with no base element.
+  --
+  -- Note: this is a partial function. On an empty 'MonoFoldable', it will
+  -- throw an exception.
+  --
+  -- /See 'Data.MinLen.ofoldr1Ex' from "Data.MinLen" for a total version of this function./
+  ofoldr1Ex f = foldr1 f . rows
+  {-# INLINE ofoldr1Ex #-}
+
+  -- | Strict left-associative fold of a monomorphic container with no base
+  -- element.
+  --
+  -- Note: this is a partial function. On an empty 'MonoFoldable', it will
+  -- throw an exception.
+  --
+  -- /See 'Data.MinLen.ofoldl1Ex'' from "Data.MinLen" for a total version of this function./
+  ofoldl1Ex' f = foldl1 f . rows
+  {-# INLINE ofoldl1Ex' #-}
