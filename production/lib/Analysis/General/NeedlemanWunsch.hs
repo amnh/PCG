eddfa6d8a7a0cefeb0a18c39e24d19a8f4db1351
Alex Washburn <awashburn@amnh.org>
2016-06-01 15:51:09 -0400

    Adding better commenting for important ordering.

--- production/lib/Analysis/General/NeedlemanWunsch.hs
+++ production/lib/Analysis/General/NeedlemanWunsch.hs
@@ -91,8 +91,14 @@ getAlignMat char1 char2 meta = result
                     leftCost            = lCost + prevCost
                     (diagChar, dgCost)  = getOverlap subChar1 subChar2 meta
                     diagCost            = diagVal + dgCost
-                    (minCost, minState, minDir) = minimumBy (comparing (\(a,_,_) -> a)) -- This order is important
-                                                        [(leftCost, leftChar, LeftDir), (downCost, downChar, DownDir), (diagCost, diagChar, DiagDir)]
+                    (minCost, minState, minDir) = minimumBy (comparing (\(a,_,_) -> a))
+                                                -- This order is important!
+                                                -- In the event of equal cost we want to
+                                                -- prioritize elements earlier in the list.
+                                                [ (leftCost, leftChar, LeftDir)
+                                                , (downCost, downChar, DownDir)
+                                                , (diagCost, diagChar, DiagDir)
+                                                ]
 
 -- | Performs the traceback of an alignment matrix
 -- Takes in an alignment matrix, two sequences, and the alphabet length
