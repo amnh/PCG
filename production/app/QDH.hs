c337c21670a0f0fc37307adbe56bf98db54899fc
Alex Washburn <awashburn@amnh.org>
2016-06-21 11:28:31 -0400

    Creating a more robust test harness as QDH.

--- production/app/QDH.hs
+++ production/app/QDH.hs
@@ -1,17 +1,63 @@
 module Main (main) where
 
---import Data.Bifunctor
-import Control.Evaluation
-import PCG.Computation.Internal
-import PCG.Script
-import Text.Megaparsec
---main = undefined
-{--}
+import Analysis.ImpliedAlignment.DynamicProgramming
+import Analysis.Parsimony.Binary.Optimization
+import Control.Applicative ((<|>))
+import Data.Vector         ((!))
+import Safe                (headMay, readMay, tailMay)
+import System.Environment  (getArgs)
+import Test.Custom
+
+import Debug.Trace (trace)
+
 main :: IO ()
-main = getContents
-   >>= checkInput . parse scriptStreamParser "STDIN stream"
-   where
-     checkInput (Left  err) = print err
-     checkInput (Right val) = runEvaluation (evaluate =<< (state . evalEither . interpret) val)
-                          >>= print
+main = getArgs
+   >>= compute . handleInput
+
+handleInput :: [String] -> SimpleTree
+handleInput args =
+  case trySimpleTree args <|> trySimpleBinaryTree args of
+    Just x  -> x
+    Nothing -> exampleTree
+
+compute :: SimpleTree -> IO ()
+compute = print . numeration (defMeta ! 0) . allOptimization 0 defMeta 
+
+trySimpleTree :: [String] -> Maybe SimpleTree
+trySimpleTree xs = do
+  rootIndex <- readMay =<< headMay xs :: Maybe Int
+  alphabet  <- readMay =<< headMay =<< tailMay xs :: Maybe String
+  mapping   <- readMay =<< headMay =<< tailMay =<< tailMay xs :: Maybe [(Int,String,[Int])]
+  pure $ createSimpleTree rootIndex alphabet mapping
+
+trySimpleBinaryTree :: [String] -> Maybe SimpleTree
+trySimpleBinaryTree xs = do
+  leafValues <- readMay =<< headMay xs :: Maybe [String]
+  pure $ createBinary leafValues
+
+exampleTree :: SimpleTree
+exampleTree = createSimpleTree 8 "ACTG"
+  [ (2,    "",[0,1])
+  , (0, "ACG",   [])
+  , (1,"ACGT",   [])
+  , (3,"ACTA",   [])
+  , (4,    "",[2,3])
+  , (5, "ACT",   [])
+  , (6,    "",[4,5])
+  , (7, "ACA",   [])
+  , (8,    "",[6,7])
+  ]
 {--}
+{--
+     $ createSimpleTree 0 "ACTG"
+     [ (0,     "", [1,2])
+     , (1,     "", [3,4])
+     , (2,     "", [5,6])
+     , (3,     "", [7,8])
+     , (4,  "ACA",    [])
+     , (5, "ACTA",    [])
+     , (6, "ACGT",    [])
+     , (7,  "ACG",    [])
+     , (8,  "ACT",    [])
+     ]
+--}
