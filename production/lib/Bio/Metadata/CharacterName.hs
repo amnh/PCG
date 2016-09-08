7f91a7d4580bb5f0abf617d87105ae7b316c80ef
Alex Washburn <awashburn@amnh.org>
2016-09-08 16:00:18 -0400

    Restricted CharacterName outputs. Stubbed out smart constructor. Added documentation.

--- production/lib/Bio/Metadata/CharacterName.hs
+++ production/lib/Bio/Metadata/CharacterName.hs
@@ -13,28 +13,29 @@
 -----------------------------------------------------------------------------
 
 module Bio.Metadata.CharacterName
-  ( CharacterName(..)
+  ( CharacterName()
+  , isUserDefined
+  , makeCharacterNames
+  , sourceFile
   ) where
+
+import Data.List   (isPrefixOf)
+import Data.Monoid
+import Text.Show   (showListWith, showString)
 
 data CharacterName
    = UserDefined FilePath String
    | Default     FilePath Int
    deriving (Eq)
 
-sourceFile :: CharacterName -> FilePath
-sourceFile (UserDefined x _) = x
-sourceFile (Default     x _) = x
-
-makeUserDefinedCharacterName :: FilePath -> String -> CharacterName
-makeUserDefinedCharacterName = UserDefined
-
-makeDefaultCharacterName :: FilePath -> Int -> CharacterName
-makeDefaultCharacterName = Default
-
 instance Show CharacterName where
   show (UserDefined _ name) = name
   show (Default path index) = path <> ":" <> show index
 
+  showList = showListWith f
+    where
+      f x = showString $ "\"" <> show x <> "\""
+ 
 instance Ord CharacterName where
   lhs@Default{} `compare` rhs@UserDefined{} =
     case rhs `compare` lhs of
@@ -42,8 +43,117 @@ instance Ord CharacterName where
       GT -> LT
       EQ -> EQ
   lhs@(UserDefined _ name) `compare` rhs@(Default path index)
-    | path <> ":" `isPrefixOf` name = LT
+    | (path <> ":") `isPrefixOf` name = LT
     | otherwise = strCmp lhs rhs
-  compare = strCmp
+  compare lhs rhs = strCmp lhs rhs
 
+-- Used internally for orderign logic after special cases are checked.
+strCmp :: Show a => a -> a -> Ordering
 strCmp lhs rhs = show lhs `compare` show rhs
+
+
+
+-- | Determine if the CharacterName was user defined or defaulted.
+isUserDefined :: CharacterName -> Bool
+isUserDefined UserDefined{} = True
+isUserDefined _             = False
+
+
+-- | Determine the source file for the character.
+sourceFile :: CharacterName -> FilePath
+sourceFile (UserDefined x _) = x
+sourceFile (Default     x _) = x
+
+-- | Construct many 'CharacterName's for the input list of '(FilePath, Maybe String)' pairs.
+--   'Just' values represent user supplied character names while 'Nothing' values represent
+--   character names to be defaulted. However, in some cases 'Just' valued strings may be
+--   rejected and defaulted instead. This occurs when the user defined character name starts
+--   with a ":" or is empty.
+--
+--   The construction is order preserving on the input structure. Multiple characters from a
+--   ffrom the same input file will be defualted with incrementing indices.
+--
+-- ==== __Examples__
+--
+-- Basic usage:
+--
+-- >>> makeCharacterNames [("file.dat", Nothing)]
+-- ["file.dat:0"]
+--
+-- >>> makeCharacterNames [("file.dat", Just "Eyes")]
+-- ["Eyes"]
+--
+-- >>> makeCharacterNames [("path/to/file.dat", Nothing")]
+-- ["file.dat:0"]
+--
+-- >>> makeCharacterNames [("path/to/file.dat", Just "Eyes")]
+-- ["Eyes"]
+--
+-- >>> makeCharacterNames [("file.dat", Nothing), ("file.dat", Nothing)]
+-- ["file.dat:0","file.dat:1"]
+--
+-- >>> makeCharacterNames [("file.dat", Nothing), ("file.dat", Just "Eyes"), ("file.dat", Nothing)]
+-- ["file.dat:0","Eyes","file.dat:1"]
+--
+-- >>> makeCharacterNames [("foo.txt", Nothing), ("foo.tx", Nothing), ("bar.txt", Nothing"), ("baz.txt",Nothing), ("baz.txt",Nothing)]
+-- ["foo.txt:0","foo.txt:1","bar.txt:0","baz.txt:0","baz.txt"]
+--
+-- >>> makeDefaultCharacterNameRange ("virus.exe", Just "")
+-- ["virus.exe:0"]
+--
+-- >>> makeDefaultCharacterNameRange ("virus.exe", Just ":")
+-- ["virus.exe:0"]
+--
+-- >>> makeDefaultCharacterNameRange ("virus.exe", Just ":a")
+-- ["virus.exe:0"]
+--
+-- >>> makeDefaultCharacterNameRange ("virus.exe", Just "a:")
+-- ["a:"]
+--
+-- >>> makeDefaultCharacterNameRange ("virus.exe", Just "!:%a")
+-- ["!:%a"]
+--
+-- >>> makeCharacterNames [("foo.txt", Nothing), ("foo.tx", Just ""), ("foo.tx", Nothing)]
+-- ["foo.txt:0","foo.txt:1","foo.txt:2"]
+-- 
+makeCharacterNames :: (Foldable f, Functor f,) => f (FilePath, Maybe String) => f CharacterName
+makeCharacterNames xs = undefined
+{-
+-- | Constructor for a 'CharacterName' that has been specified explicitly by user input.
+makeUserDefinedCharacterName :: FilePath -> String -> CharacterName
+makeUserDefinedCharacterName path name
+  | null path = makeDefaultCharacterName path 
+  | null name = 
+  | = UserDefined
+
+-- | Constructor for a 'CharacterName' that has needs to be defaulted.
+-- Please don't pass in an 
+makeDefaultCharacterName :: FilePath -> Int -> CharacterName
+makeDefaultCharacterName = Default
+
+-- | Construct many sequentially indexed default 'CharacterName's for the suppleid range and 'FilePath'.
+--
+-- ==== __Examples__
+--
+-- Basic usage:
+--
+-- >>> makeDefaultCharacterNameRange "path/to/file.dat" 3 5
+-- ["file.dat:3","file.dat:4","file.dat:5"]
+--
+-- >>> makeDefaultCharacterNameRange "foo.bar" 1 1
+-- ["foo.bar:1"]
+--
+-- >>> makeDefaultCharacterNameRange "virus.exe" (-6) 2
+-- ["virus.exe:0","virus.exe:1",virus.exe:2"]
+--
+-- >>> makeDefaultCharacterNameRange "neg.val" (-3) (-1)
+-- []
+--
+-- >>> makeDefaultCharacterNameRange "high.low" 8 6
+-- []
+-- 
+makeDefaultCharacterNameRange :: FilePath -> Int -> Int -> [CharacterName]
+makeDefaultCharacterNameRange path lower upper
+  | upper < 0 = []
+  | otherwise = Default path <$> [max lower 0 .. upper]
+-}
