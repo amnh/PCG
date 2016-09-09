9b25c60fbf91093b17802264cec0fc6bee742d21
Alex Washburn <awashburn@amnh.org>
2016-09-09 16:58:36 -0400

    Updated Alphabet type to support state names corresponding to alphabet symbols.

--- production/lib/Data/Alphabet.hs
+++ production/lib/Data/Alphabet.hs
@@ -8,12 +8,21 @@
 -- Stability   :  provisional
 -- Portability :  portable
 --
--- An 'Alphabet' represents an /alphabetically ordered/ list of unique symbols
--- with constant time random access. Symbols are any data type which are
--- coercable from a 'String' through the 'IsString' type-class.
+-- An 'Alphabet' represents an /ordered/ list of unique symbols with constant
+-- time random access. Symbols are any data type which are coercable from a
+-- 'String' through the 'IsString' type-class.
 --
--- An 'Alphabet' is constructed by supplying a `Foldable` structure of symbols
--- which are 'IsString' instances to the 'constructAlphabet' function.
+-- An 'Alphabet' is constructed in one of two ways:
+--
+--  1. Supplying a `Foldable` structure of symbols which are 'IsString'
+--     instances to the 'fromSymbols' function.
+--
+--  2. Supplying a `Foldable` structure of symbols and state name pairs,
+--     both of which are 'IsString' instances to the 'fromSymbolsWithStateNames'
+--     function.
+--
+-- Both 'Alphabet' construction methods are order preserving with respect to the
+-- input symbol order.
 --
 -- Every 'Alphabet' contains a "gap" symbol denoted by the expression:
 -- > fromString "-"
@@ -26,23 +35,25 @@
 -- This symbol will be removed from the 'Alphabet'
 -- if it is present in the construction structure.
 -----------------------------------------------------------------------------   
-{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE FlexibleContexts, FlexibleInstances, TypeFamilies #-}
 
 module Data.Alphabet
-  ( Alphabet(..)
-  , constructAlphabet
-  , constructAlphabetWithTCM
+  ( Alphabet()
+  , alphabetStateNames
+  , alphabetSymbols
+  , fromSymbols
+  , fromSymbolsWithStateNames
   , gapCharacter
   ) where
 
+import           Control.Monad.State.Strict
 import           Data.Foldable
 import           Data.Key
-import           Data.List                    (elemIndex, intercalate)
+import           Data.List                    (elemIndex, intercalate, sort)
 import           Data.Matrix.NotStupid        (Matrix, matrix)
 import           Data.Maybe
 import           Data.Monoid
-import           Data.Set                     (delete)
-import           Data.Set              as Set (fromList)
+import           Data.Set                     (insert)
 import           Data.String
 import           Data.Vector                  (Vector, generate)
 import qualified Data.Vector           as V
@@ -55,10 +66,9 @@ import           Test.QuickCheck.Arbitrary.Instances ()
    from either parsers or in unification step.
  -}
 
--- | Newtyped to ensure invariants are preserved.
-newtype Alphabet a
-      = Alphabet (Vector a)
-      deriving (Eq)
+data Alphabet a 
+   = SimpleAlphabet     (Vector (UnnamedSymbol a))
+   | StateNamedAlphabet (Vector (  NamedSymbol a))
 
 type instance Key Alphabet = Int
 
@@ -74,62 +84,153 @@ instance Indexable Alphabet where
                  , "]."
                  ]
 
+
 instance Lookup Alphabet where
   {-# INLINE lookup #-}
-  lookup i (Alphabet v) = v V.!? i
+  lookup i alphabet = symbolVector alphabet V.!? i
 
 
 instance Foldable Alphabet where
   {-# INLINE foldr #-}
-  foldr  f e (Alphabet v) = V.foldr f e v
+  foldr  f e = V.foldr  f e . symbolVector
 
   {-# INLINE foldl #-}
-  foldl  f e (Alphabet v) = V.foldl f e v
+  foldl  f e = V.foldl  f e . symbolVector
 
   {-# INLINE foldr1 #-}
-  foldr1 f (Alphabet v) = V.foldr1 f v
+  foldr1 f   = V.foldr1 f   . symbolVector
 
   {-# INLINE foldl1 #-}
-  foldl1 f (Alphabet v) = V.foldl1 f v
+  foldl1 f   = V.foldl1 f   . symbolVector
 
   {-# INLINE length #-}
-  length (Alphabet v) = V.length v
+  length = V.length . symbolVector
 
 
 instance FoldableWithKey Alphabet where
   {-# INLINE foldrWithKey #-}
-  foldrWithKey f e (Alphabet v) = V.ifoldr' f e v
+  foldrWithKey f e = V.ifoldr' f e . symbolVector
 
   {-# INLINE foldlWithKey #-}
-  foldlWithKey f e (Alphabet v) = V.ifoldl' f e v
+  foldlWithKey f e = V.ifoldl' f e . symbolVector
+
+
+-- | /O(n * log n)/
+instance Ord a => Eq (Alphabet a) where
+  lhs == rhs =  length lhs == length rhs
+             && sort (toList lhs) == sort (toList rhs)
 
 
 instance Show a => Show (Alphabet a) where
-  show (Alphabet v) = mconcat [ "Alphabet: {"
-                               , intercalate ", " $ show <$> toList v
-                               , "}"
-                               ]
+  show x = mconcat [ "Alphabet: {"
+                   , intercalate ", " $ show <$> toList x
+                   , "}"
+                   ]
 
 instance (Arbitrary a, Ord a, IsString a) => Arbitrary (Alphabet a) where
-  arbitrary = constructAlphabet <$> listOf1 arbitrary
+  arbitrary = fromSymbols <$> listOf1 arbitrary
 
--- | Constructs an 'Alphabet' from a 'Foldable' structure of 'IsString' values.
+
+-- | Constructs an 'Alphabet' from a 'Foldable' structure of symbols which are
+--   'IsString' values.
 --
---   /O(n*log(n))/
-constructAlphabet :: (Ord a, IsString a, Foldable t) => t a -> Alphabet a
-constructAlphabet = Alphabet . V.fromList . appendGapSymbol . toList . removeSpecialSymbols . Set.fromList . toList
-  where
-    gapSymbol            = fromString "-"
-    missingSymbol        = fromString "?"
-    appendGapSymbol      = (<> [gapSymbol])
-    removeSpecialSymbols = delete missingSymbol . delete gapSymbol
-    
+--   /O(n * log n)/
+fromSymbols :: (Ord a, IsString a, Foldable t) => t a -> Alphabet a
+fromSymbols = SimpleAlphabet . fmap (Unnamed . toSingle) . alphabetPreprocessing . fmap fromSingle . toList
+
+
+-- | Constructs an 'Alphabet' from a 'Foldable' structure of symbols and
+--   coresponding state names, both of which a are 'IsString' values.
+--
+--   The input ordering is preserved.
+--
+--   /O(n * log n)/
+fromSymbolsWithStateNames :: (Ord a, IsString a, Foldable t) => t (a,a) -> Alphabet a
+fromSymbolsWithStateNames = StateNamedAlphabet . fmap (Named . toTuple) . alphabetPreprocessing . fmap fromTuple . toList
+
+
+-- | Retreives the symbols of the 'Alphabet'. Synonym for 'toList'.
+--
+-- /O(n)/
+alphabetSymbols :: Alphabet a -> [a]
+alphabetSymbols = toList
+
+
+-- | Retreives the state names for the symbols of the 'Alphabet'.
+--
+-- If there the symbols of the 'Alphabet' were not given state names during
+-- construction then an empty list is returned.
+--
+-- /O(n)/
+alphabetStateNames :: Alphabet a -> [a]
+alphabetStateNames      SimpleAlphabet{}  = []
+alphabetStateNames (StateNamedAlphabet v) = toList $ snd . fromNamed <$> v
+
+
 -- | Retreives the "gap character" from the alphabet.
 --
 --   /O(1)/
 gapCharacter :: Alphabet a -> a
 gapCharacter alphabet = alphabet ! (length alphabet - 1)
 
+
+
+{-
+ -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+ -   Supporting code and data structures:
+ -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+ -}
+
+
+
+alphabetPreprocessing :: (Ord a, InternalClass a, Foldable t) => t a -> Vector a
+alphabetPreprocessing = V.fromList . appendGapSymbol . removeSpecialSymbolsAndDuplicates . toList
+  where
+    appendGapSymbol       = (<> [gapSymbol])
+    removeSpecialSymbolsAndDuplicates = (`evalState` mempty) . filterM f
+      where
+        f x
+          | isGapSymboled     x = pure False
+          | isMissingSymboled x = pure False
+          | otherwise           = do
+              seenSet <- get
+              _       <- put $ x `insert` seenSet
+              pure $ x `notElem` seenSet
+
+newtype UnnamedSymbol a = Unnamed  a
+newtype NamedSymbol   a = Named (a,a)
+
+fromUnnamed (Unnamed x) = x
+fromNamed   (Named   x) = x
+
+symbolVector (SimpleAlphabet     v) =       fromUnnamed <$> v
+symbolVector (StateNamedAlphabet v) = fst . fromNamed   <$> v
+
+-- Newtypes for corecing and consolidation of alphabet input processing logic
+newtype AlphabetInputSingle a = ASI  { toSingle ::  a    } deriving (Eq,Ord)
+newtype AlphabetInputTuple  a = ASNI { toTuple  :: (a,a) } deriving (Eq,Ord)
+
+fromSingle = ASI
+fromTuple  = ASNI
+
+class InternalClass a where
+  gapSymbol         :: a
+  isGapSymboled     :: a -> Bool
+  isMissingSymboled :: a -> Bool
+
+instance (Eq a, IsString a) => InternalClass (AlphabetInputSingle a) where
+  gapSymbol         = ASI $ fromString "-"
+  isGapSymboled     = (gapSymbol ==)
+  isMissingSymboled = (ASI (fromString "?") ==)
+
+instance (Eq a, IsString a) => InternalClass (AlphabetInputTuple a) where
+  gapSymbol                      = ASNI (fromString "-", fromString "-")
+  isGapSymboled     (ASNI (x,_)) = x == fromString "-"
+  isMissingSymboled (ASNI (x,_)) = x == fromString "?"
+
+
+{-
+    
 -- | Constructs an 'Alphabet' with a corresponding TCM. Permutes TCM rows and
 --   columns as the 'Alphabet' is reordered. Deletes TCM rows and columns where
 --   'Alphabet' symbols are eliminated.
@@ -145,3 +246,5 @@ constructAlphabetWithTCM symbols originalTcm = (alphabet, permutedTcm)
     oldOrdering = generate len (\x -> fromJust $ (alphabet ! x) `elemIndex` toList symbols)
     permutedTcm = matrix len len f
     f (i,j) =  originalTcm ! (oldOrdering V.! i, oldOrdering V.! j)
+
+-}
