b3ea4e995206dd46da21e53da9715b025f88c909
Alex Washburn <awashburn@amnh.org>
2016-09-28 17:55:30 -0400

    Adding datastructures for sequence binning.

---
+++ production/lib/Bio/Metadata/Sequence.hs
@@ -0,0 +1,18 @@
+------------------------------------------------------------------------------
+-- |
+-- Module      :  Bio.Metadata.Sequence
+-- Copyright   :  (c) 2015-2015 Ward Wheeler
+-- License     :  BSD-style
+--
+-- Maintainer  :  wheeler@amnh.org
+-- Stability   :  provisional
+-- Portability :  portable
+--
+-----------------------------------------------------------------------------
+
+{-# LANGUAGE FlexibleContexts #-}
+
+module Bio.Metadata.Sequence
+  (
+  ) where
+
