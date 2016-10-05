33f885489cc8e8cb33818215fde937dd47e79f50
Alex Washburn <awashburn@amnh.org>
2016-10-05 11:44:43 -0400

    Improved code formatting.

--- production/lib/Analysis/ImpliedAlignment/Test.hs
+++ production/lib/Analysis/ImpliedAlignment/Test.hs
@@ -36,19 +36,20 @@ import           Test.Tasty
 import           Test.Tasty.HUnit
 import           Test.Tasty.QuickCheck
 
+
 defMeta :: Vector (CharacterMetadata s)
 defMeta = pure CharMeta
-        { charType   = DirectOptimization
-        , alphabet   = undefined
-        , name       = "DefaultCharacter"
-        , isAligned  = False
-        , isIgnored  = False
-        , weight     = 1.0
-        , stateNames = mempty
-        , fitchMasks = undefined
-        , rootCost   = 0.0
-        , costs      = GeneralCost { indelCost = 2, subCost = 1 }
-        }
+    { charType   = DirectOptimization
+    , alphabet   = undefined
+    , name       = "DefaultCharacter"
+    , isAligned  = False
+    , isIgnored  = False
+    , weight     = 1.0
+    , stateNames = mempty
+    , fitchMasks = undefined
+    , rootCost   = 0.0
+    , costs      = GeneralCost { indelCost = 2, subCost = 1 }
+    }
 
 testSuite :: TestTree
 testSuite = testGroup "Implied Alignment"
@@ -69,7 +70,6 @@ fullIA = testGroup "Full alignment properties"
     ]
   where
     lenHoldsTest = testProperty "The sequences on a tree are longer or the same at end." checkLen
-
     checkLen :: SimpleTree -> Bool
     checkLen inputTree = nodeInvariantHolds impliedAlignmentLengthIsLonger outputTree
       where
@@ -81,55 +81,59 @@ fullIA = testGroup "Full alignment properties"
 
 testNumerate :: TestTree
 testNumerate = testGroup "Numeration properties"
-      [ idHolds
-      , lengthHolds
-      , counterIncrease
-      , monotonic
-      ]
-    where
-        idHolds                          = testProperty "When a sequence is numerated with itself, get indices and the same counter" checkID
-        checkID :: DynamicChar -> Bool
-        checkID inChar                   = onull inChar || (traces == defaultH && counter <= olength inChar)
-            where
-                defaultH = V.fromList [0..olength inChar - 1] 
-                (traces, (_, counter), _) =  numerateOne inChar inChar (0, 0)
+    [ idHolds
+    , lengthHolds
+    , counterIncrease
+    , monotonic
+    ]
+  where
+    idHolds                          = testProperty "When a sequence is numerated with itself, get indices and the same counter" checkID
+    checkID :: DynamicChar -> Bool
+    checkID inChar                   = onull inChar || (traces == defaultH && counter <= olength inChar)
+      where
+        defaultH = V.fromList [0..olength inChar - 1] 
+        (traces, (_, counter), _) =  numerateOne inChar inChar (0, 0)
 
-        -- TODO: Talk to Eric about olength ()
-        lengthHolds                      = testProperty "Numerate returns a sequence of the correct length" checkLen
-        checkLen :: (GoodParsedChar, GoodParsedChar) -> Int -> Bool
-        checkLen inParse count           = V.length traces >= maxLen
-            where 
-                (seq1, seq2)              = encodeArbSameLen inParse
-                (traces, (_, _), _) = numerateOne seq1 seq2 (olength seq1, count)
-                maxLen                    = maximum [olength seq1, olength seq2]
+    -- TODO: Talk to Eric about olength ()
+    lengthHolds                      = testProperty "Numerate returns a sequence of the correct length" checkLen
+    checkLen :: (GoodParsedChar, GoodParsedChar) -> Int -> Bool
+    checkLen inParse count           = V.length traces >= maxLen
+      where 
+         (seq1, seq2)        = encodeArbSameLen inParse
+         (traces, (_, _), _) = numerateOne seq1 seq2 (olength seq1, count)
+         maxLen              = maximum [olength seq1, olength seq2]
 
-        counterIncrease                   = testProperty "After numerate runs, counter is same or larger" checkCounter
-        checkCounter :: (GoodParsedChar, GoodParsedChar) -> Int -> Bool
-        checkCounter inParse count        = counter >= count
-            where 
-                (seq1, seq2)              = encodeArbSameLen inParse
-                (_, (_, counter), _) = numerateOne seq1 seq2 (olength seq1, count)
-        monotonic = testProperty "Numerate produces a monotonically increasing homology" checkIncrease
-        checkIncrease :: (GoodParsedChar, GoodParsedChar) -> Int -> Bool
-        checkIncrease inParse count       = increases $ toList traces
-            where 
-                (seq1, seq2)         = encodeArbSameLen inParse
-                (traces, _, _) = numerateOne seq1 seq2 (olength seq1, count)
-                increases :: Ord a => [a] -> Bool
-                increases []         = True
-                increases [_]        = True
-                increases (x:y:xs)   = x < y && increases (y:xs)
+    counterIncrease                   = testProperty "After numerate runs, counter is same or larger" checkCounter
+    checkCounter :: (GoodParsedChar, GoodParsedChar) -> Int -> Bool
+    checkCounter inParse count        = counter >= count
+      where 
+        (seq1, seq2)              = encodeArbSameLen inParse
+        (_, (_, counter), _) = numerateOne seq1 seq2 (olength seq1, count)
+
+    monotonic = testProperty "Numerate produces a monotonically increasing homology" checkIncrease
+    checkIncrease :: (GoodParsedChar, GoodParsedChar) -> Int -> Bool
+    checkIncrease inParse count       = increases $ toList traces
+      where 
+        (seq1, seq2)         = encodeArbSameLen inParse
+        (traces, _, _) = numerateOne seq1 seq2 (olength seq1, count)
+        increases :: Ord a => [a] -> Bool
+        increases []         = True
+        increases [_]        = True
+        increases (x:y:xs)   = x < y && increases (y:xs)
+
 
 -- | Useful function to convert encoding information to two encoded seqs
 encodeArbSameLen :: (GoodParsedChar, GoodParsedChar) -> (DynamicChar, DynamicChar)
-encodeArbSameLen (parse1, parse2) = ( encodeStream alph . NE.fromList . fmap NE.fromList . toList $ V.take minLen p1
-                                    , encodeStream alph . NE.fromList . fmap NE.fromList . toList $ V.take minLen p2
-                                    )
-    where
-        (p1,p2) = (getGoodness parse1, getGoodness parse2)
-        minLen  = minimum [length p1, length p2]
-        oneAlph = foldMap S.fromList
-        alph    = fromSymbols $ oneAlph p1 `S.union` oneAlph p2
+encodeArbSameLen (parse1, parse2) =
+    ( encodeStream alph . NE.fromList . fmap NE.fromList . toList $ V.take minLen p1
+    , encodeStream alph . NE.fromList . fmap NE.fromList . toList $ V.take minLen p2
+    )
+  where
+    (p1,p2) = (getGoodness parse1, getGoodness parse2)
+    minLen  = minimum [length p1, length p2]
+    oneAlph = foldMap S.fromList
+    alph    = fromSymbols $ oneAlph p1 `S.union` oneAlph p2
+
 
 -- | Newtyping ensures that the sequence and ambiguity groups are both non empty.
 newtype GoodParsedChar
@@ -137,12 +141,13 @@ newtype GoodParsedChar
       { getGoodness :: ParsedChar
       } deriving (Eq,Show)
 
+
 instance Arbitrary GoodParsedChar where
-  arbitrary = do
-    symbols                     <- getNonEmpty <$> arbitrary :: Gen [String]
-    let ambiguityGroupGenerator =  sublistOf symbols `suchThat` (not . null)
-    someAmbiguityGroups         <- V.fromList <$> listOf1 ambiguityGroupGenerator
-    pure $ GoodParsedChar someAmbiguityGroups
+    arbitrary = do
+        symbols                     <- getNonEmpty <$> arbitrary :: Gen [String]
+        let ambiguityGroupGenerator =  sublistOf symbols `suchThat` (not . null)
+        someAmbiguityGroups         <- V.fromList <$> listOf1 ambiguityGroupGenerator
+        pure $ GoodParsedChar someAmbiguityGroups
 
 
 
