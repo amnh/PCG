d180d03684a9158c1644e08a3d0468c034a72882
Alex Washburn <awashburn@amnh.org>
2016-10-07 21:16:36 -0400

    Mocked out Decoration file.

---
+++ production/lib/Bio/Character/Dynamic/Decoration/Internal.hs
@@ -0,0 +1,82 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Bio.Character.Dynamic.Decoration.Internal
+-- Copyright   :  (c) 2015-2015 Ward Wheeler
+-- License     :  BSD-style
+--
+-- Maintainer  :  wheeler@amnh.org
+-- Stability   :  provisional
+-- Portability :  portable
+--
+-----------------------------------------------------------------------------
+
+{-# LANGUAGE TypeFamilies #-}
+
+module Bio.Character.Dynamic.Decoration.Internal where
+
+--import           Bio.Character.Dynamic
+--import           Bio.Character.Static
+import           Bio.Sequence.Block   (CharacterBlock)
+import           Data.Foldable
+import           Data.List.NonEmpty   (NonEmpty)
+import           Data.MonoTraversable
+
+
+data CharacterSequence s d
+   = CharSeq (NonEmpty (CharacterBlock s d))
+
+
+{-# INLINE toBlocks #-}
+toBlocks :: CharacterSequence s d -> NonEmpty (CharacterBlock s d)
+toBlocks (CharSeq x) = x
+
+
+{-# INLINE fromBlocks #-}
+fromBlocks :: NonEmpty (CharacterBlock s d) -> CharacterSequence s d
+fromBlocks = CharSeq
+
+
+type instance Element (CharacterSequence s d) = CharacterBlock s d
+
+
+instance MonoFunctor (CharacterSequence s d) where
+
+    {-# INLINE omap #-}
+    omap f = fromBlocks . fmap f . toBlocks
+
+
+instance MonoFoldable (CharacterSequence s d) where
+
+    {-# INLINE ofoldMap #-}
+    ofoldMap f = foldMap f . toBlocks
+
+    {-# INLINE ofoldr #-}
+    ofoldr f e = foldr f e . toBlocks
+
+    {-# INLINE ofoldl' #-}
+    ofoldl' f e = foldl' f e . toBlocks
+
+    {-# INLINE ofoldr1Ex #-}
+    ofoldr1Ex f = foldr1 f . toBlocks
+
+    {-# INLINE ofoldl1Ex' #-}
+    ofoldl1Ex' f = foldl1 f . toBlocks
+
+    {-# INLINE onull #-}
+    onull = const False
+
+    {-# INLINE olength #-}
+    olength = length . toBlocks
+
+
+-- | Monomorphic containers that can be traversed from left to right.
+instance MonoTraversable (CharacterSequence s d) where
+
+    {-# INLINE otraverse #-}
+    otraverse f = fmap fromBlocks . traverse f . toBlocks
+
+    {-# INLINE omapM #-}
+    omapM = otraverse
+
+
+class 
