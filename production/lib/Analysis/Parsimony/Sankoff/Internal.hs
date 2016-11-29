a381f0e00d198868ec6a18d6483f2752185abde1
Eric Ford <eric@twistcollective.com>
2016-11-29 16:27:48 -0500

    Started adding MathJax stuff into Sankoff/Internal.hs.

--- production/lib/Analysis/Parsimony/Sankoff/Internal.hs
+++ production/lib/Analysis/Parsimony/Sankoff/Internal.hs
@@ -29,12 +29,19 @@ sankoffPostOrder charDecoration childDecorations =
         then initializeCostVector charDecoration
         else map updateCostVector charDecoration childDecorations
 
+-- | Used on the pre-order (i.e. second) traversal.
 sankoffPreOrder  :: ( HasTCM d ((c, c) -> (c, Double))
                     , HasStaticCharacter d c
                     , EncodableStaticCharacter c
                     ) => (d -> [d'] -> d')
 sankoffPreOrder = undefined
 
+-- | Before post-order can actually occur, must initialize leaf vectors with values as such:
+-- Given \(n\) character states, for a given character \(i_c\) on leaf \(i\), there are \(2^n - 1)
+-- possible characters, including ambiguous characters. For extant character states \(i_c_x\) on
+-- the leaf, and for each possible character state, \(i\)
+-- \[ cost(i_c) =
+--       \] \(i \elem s_x\)
 initializeCostVector :: EncodableStaticCharacter c => c -> SankoffCharacterDecoration c
 initializeCostVector inputChar = returnChar
     where
