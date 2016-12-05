6b19c163d9dca6c9622a9170464fbc46b7e4b8c6
Eric Ford <eric@twistcollective.com>
2016-12-05 16:02:55 -0500

    Still working on Sankoff/Internal. No significant progress to report, sadly.

--- production/lib/Analysis/Parsimony/Sankoff/Internal.hs
+++ production/lib/Analysis/Parsimony/Sankoff/Internal.hs
@@ -10,24 +10,28 @@
 --
 -- Sankoff character analysis (cost and median)
 --
+-- This only works on static characters, and due to the traversal, only one
+-- character will be received at a time.
+--
 -----------------------------------------------------------------------------
+
+import Control.Lens
+import Bio.Character.Decoration.Discrete
+import Bio.Character.Encodable
 
 data SankoffCharacterDecoration c = SankoffCharacterDecoration
     { minCostVector :: [Word32]
-    , dirVectors    :: [c]
+    , dirVector     :: [(Bool, Bool)] -- for a character state, (from left, from right)
     , minCost       :: Word32
     }
 
 -- | Used on the post-order (i.e. first) traversal.
 --
-sankoffPostOrder :: ( HasTCM d ((c, c) -> (c, Double))
-                    , HasStaticCharacter d c
-                    , EncodableStaticCharacter c
-                    ) => (d -> [d'] -> d')
+sankoffPostOrder :: ( EncodableStaticCharacter c, DiscreteCharacterDecoration d c ) => (d -> [d'] -> d')
 sankoffPostOrder charDecoration childDecorations =
     if isLeaf charDecoration
         then initializeCostVector charDecoration
-        else map updateCostVector charDecoration childDecorations
+        else updateCostVector charDecoration childDecorations -- childDecorations needs to be a tuple
 
 -- | Used on the pre-order (i.e. second) traversal.
 sankoffPreOrder  :: ( HasTCM d ((c, c) -> (c, Double))
@@ -43,27 +47,40 @@ sankoffPreOrder = undefined
 -- \[ cost(i_c) =
 --       \] \(i \elem s_x\), etc...
 -- TODO: finish above comment once MathJax is working
-initializeCostVector :: EncodableStaticCharacter c => c -> SankoffCharacterDecoration c
-initializeCostVector inputChar = returnChar
+initializeCostVector :: DiscreteCharacterDecoration d c => c -> SankoffCharacterDecoration c
+initializeCostVector inputDecoration = returnChar
     where
         -- assuming metricity and 0 diagonal
-        costList = foldMapWithKey f $ getAlphabet inputChar
+        costList = foldMapWithKey f $ inputDecoration ^. characterAlphabet
             where
                 f key alphState
                     | key `testBit` inputChar = [minBound :: Word32]
                     | otherwise               = [maxBound :: Word32] -- Change this if it's actually Doubles.
+                    where inputChar = (inputDecoration ^. discreteCharacter)
         returnChar = SankoffCharacterDecoration costList [] (minBound :: Word32)
 
-updateCostVector :: EncodableStaticCharacter c => c -> SankoffCharacterDecoration c -> SankoffCharacterDecoration c
-updateCostVector curDecoration childDecoration = returnChar
+-- | Given current node and its children, does actual calculation of new node value:
+-- [overall minimum cost, [minimum cost for character state], [direction arrows]],
+-- for each character state, for each character on current node. Does not assume binary tree.
+updateCostVector :: EncodableStaticCharacter c => c -> (SankoffCharacterDecoration c, SankoffCharacterDecoration c) -> SankoffCharacterDecoration c
+updateCostVector curNodeDecoration []      = undefined
+updateCostVector curNodeDecoration (x:[])  = undefined
+updateCostVector curNodeDecoration (x:y:_) = returnNodeDecoration
     where
-        minCost, costVector, dirVector = foldl f [] [] $ getAlphabet curDecoration -- fold over alphabet states, i
-            where
-                f alphIdxI costList dirList = foldl g getAlphabet  [maxBound :: Word32] curDecoration -- fold over alphabet states, j
-                    where
-                        g curMin alphIdxJ = finalMin costList dirList
+{-        -- fold over characters
+        costTuple = foldl (\x acc -> (fst acc : overallMin, snd acc : perCharStateMin)
                             where
-                                curMin =
+                                (overallMin, perCharStateMin) = generateSingleCharDec (getAlphabet curNodeDecoration) childCharTup
+                                dirVector = generateDirVector overallMin perCharStateMin childCharTup
+                          ) ([],[]) $ zipWith (\a b -> (getChars a, getChars b)) leftChildDec rightChildDec
+-}
+        -- There's an overall min for that character, charMin, as well as a list of mins corresponding to the minimum cost for each character state, [stateMin]
+        (minCost, costVector) = foldl (\charState (curDecoration ^. discreteCharacter, minC, costs) -> (charMin, costs : stateMin)
+                                          where
+                                              (charMin, stateMin) = calcCostPerState (curDecoration ^. discreteCharacter) minC charState
+                                      ) (maxBound :: Word32, []) $ curDecoration ^. characterAlphabet -- fold over alphabet states
+                            where
+                                finalMin =
                                     -- if minCost child[i] + tcmCost (i, j) < curMin
                                     if newMin < curMin
                                         then newMin
@@ -76,4 +93,24 @@ updateCostVector curDecoration childDecoration = returnChar
                 -- if curMin == minCost child[i] + tcmCost(i, j)
                     -- then dirVector `setBit` j
                     -- else dirVector `clearBit` j
-        returnChar = SankoffCharacterDecoration costVector dirVector minCost
+        returnChar = SankoffCharacterDecoration minCost costVector dirVector
+
+-- | Take in a single character state and two decorations (the decorations of the two child states).
+-- Return the total cost of transitioning from those two child decorations to the given character.
+--
+-- TODO: double check that I can throw away the medians here. My current thinking is that I can, because
+-- I cycle through all of the character states before this is called, so any
+calcCostPerState :: [DiscreteCharacterDecoration] -> Word32
+calcCostPerState children charState =
+    foldl (\child cost -> transitionCost + child ^. minCost
+            where
+                (tansitionCost, _) = (child ^. characterTCM) (child ^. discreteCharacter) charState
+           ) 0 children
+
+        leftCost = (median, cost) = (leftChild ^. characterTCM) (leftChild ^. discreteCharacter) + leftChild ^. minCost
+
+-- | Takes in two characters, then folds over possible character states, calculating the minimum cost of having that character state
+-- as a member of the possibly ambiguous character in the median
+generateSingleCharDec :: Alphabet -> (AlphabetChar, AlphabetChar) -> (Word32, [Word32])
+generateSingleCharDec (curDecoration, leftChildDec, rightChildDec) =
+    calcCostPerState charState [maxBound :: Word32]
