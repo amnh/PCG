56d1072eb13e6a39942aefb6db5ca956602d61f8
Eric Ford <eford@amnh.org>
2016-12-11 18:29:13 -0500

    Fitch post-order is finished.

--- production/lib/Analysis/Parsimony/Fitch/Internal.hs
+++ production/lib/Analysis/Parsimony/Fitch/Internal.hs
@@ -22,15 +22,15 @@ import Bio.Character.Decoration.Discrete
 import Bio.Character.Encodable
 
 data FitchCharacterDecoration c = FitchCharacterDecoration
-    { subtreeCost  :: Word32                                                     -- cost of the subtree
-    , median       :: (DiscreteCharacterDecoration)                              -- for a character state, its min from the left child and its min from the right
-    , childMedians :: (DiscreteCharacterDecoration, DiscreteCharacterDecoration) -- (left, right) so that we can do post order pass with all of Fitch's rules
+    { subtreeCost       :: Word32                                                     -- cost of the subtree
+    , preliminaryMedian :: (DiscreteCharacterDecoration)                              -- held here until final state is determined and we can assign that into discreteCharacter
+    , childMedians      :: (DiscreteCharacterDecoration, DiscreteCharacterDecoration) -- (left, right) so that we can do post order pass with all of Fitch's rules
     }
 
 -- | Used on the post-order (i.e. first) traversal.
 fitchPostOrder :: ( EncodableStaticCharacter c, DiscreteCharacterDecoration d c ) => (d -> [d'] -> d')
 fitchPostOrder charDecoration []               = initializeLeaf charDecoration    -- a leaf
-fitchPostOrder charDecoration childDecorations = updateNodeDec charDecoration childDecorations
+fitchPostOrder charDecoration childDecorations = updateNodeDecoration charDecoration childDecorations
 
 -- | Used on the pre-order (i.e. second) traversal. Either calls 'initializeDirVector' on root or
 -- Needs to determine which child it's updating, then sends the appropriate minlist
@@ -39,7 +39,8 @@ fitchPreOrder  :: ( HasTCM d ((c, c) -> (c, Double))
                     , EncodableStaticCharacter c
                     ) => (FitchCharacterDecoration c -> [(Word, d')] -> d')
 fitchPreOrder curDecoration (x:y:_)                             = curDecoration                     -- shouldn't be possible, but here for completion
-fitchPreOrder curDecoration []                                  = initializeDirVector curDecoration -- is a root
+fitchPreOrder curDecoration []                                  =
+    initializeDirVector curDecoration -- is a root
 fitchPreOrder curDecoration ((whichChild, parentDecoration):[]) = returnChar
     where
         returnChar
@@ -47,37 +48,31 @@ fitchPreOrder curDecoration ((whichChild, parentDecoration):[]) = returnChar
             | otherwise       = updateDirVector parentDecoration (snd (parentDecoration ^. directionalMinVector)) curDecoration -- right child
 
 
-updateNodeDec :: DiscreteCharacterDecoration d c => c -> FitchCharacterDecoration c
-updateNodeDec curDecoration childDecorations = returnChar
+calcCostAndMedian :: EncodableStaticCharacter c => c -> [FitchCharacterDecoration c] -> FitchCharacterDecoration c
+calcCostAndMedian curNodeDecoration []                       = curNodeDecoration    -- Leaf. Here for completion because levaes are filtered out before this call.
+calcCostAndMedian curNodeDecoration (x:[])                   = curNodeDecoration    -- Shouldn't be possible, but here for completion.
+calcCostAndMedian curNodeDecoration (leftChild:rightChild:_) = returnNodeDecoration
     where
-        -- assuming metricity and 0 diagonal
-        costList = foldMapWithKey f $ inputDecoration ^. characterAlphabet
-            where
-                f key alphState
-                    | key `testBit` inputChar = [minBound :: Word32]
-                    | otherwise               = [maxBound :: Word32] -- Change this if it's actually Doubles.
-                    where inputChar = (inputDecoration ^. discreteCharacter)
-        returnChar = FitchCharacterDecoration costList [] (minBound :: Word32)
+        returnNodeDecoration   = FitchCharacterDecoration cost median (leftChild ^. preliminaryMedian, rightChild ^. preliminaryMedian)
+        (cost, median) = calcCostAndMedian leftChild rightChild
+        returnVal            = foldlWithKey' f initializedAcc $ leftChildDec ^. characterAlphabet
 
--- |
--- Given current node and its children, does actual calculation of new node value
--- for each character state, for each character on current node. Assumes binary tree.
-updateCostVector :: EncodableStaticCharacter c => c -> [FitchCharacterDecoration c] -> FitchCharacterDecoration c
-updateCostVector curNodeDecoration []                       = curNodeDecoration    -- Leaf node, so shouldn't get here.
-updateCostVector curNodeDecoration (x:[])                   = curNodeDecoration    -- Shouldn't be possible, but here for completion.
-updateCostVector curNodeDecoration (leftChild:rightChild:_) = returnNodeDecoration -- _Should_ be able to amend this to use non-binary children.
-    where
-        (charCost, costVector) =
-            foldlWithKey' (\(charMin, (leftMin, rightMin)) charState _ -> (charMin, (leftMin : leftChildMin, rightMin : rightChildMin)
-                       where
-                           charMin = if stateMin < charMin
-                               then charMin
-                               else stateMin
-                           stateMin                      = leftChildMin + rightChildMin
-                           (leftChildMin, rightChildMin) =
-                               calcCostPerState charState (leftChild ^. discreteCharacter) (rightChild ^. discreteCharacter)
-                   ) (maxBound :: Word32, ([],[])) (curNodeDecoration ^. characterAlphabet)
-        returnChar = FitchCharacterDecoration costVector [] charCost
+        initializedAcc       = (emptyChar, True)
+        emptyChar            = (leftChildDec ^. discreteCharacter) `xor`     (leftChildDec ^. discreteCharacter)
+        isSet decoration key = (decoration   ^. discreteCharacter) `testBit` key
+        indel l r k          = (isSet l k) `xor` (isSet r k)
+        noSub l r k          = (isSet l k) &&    (isSet r k)
+        f (inChar, isCost) key
+            | noSub =
+                if isCost
+                    then (emptyChar `setBit` key, False)
+                    else (inChar    `setBit` key, isCost)
+            | indel leftChildDec rightChildDec key =
+                if isCost
+                    then (inChar `setBit` key, isCost)
+                    else (inChar,              isCost)
+            | otherwise  = (inChar, isCost)
+
 
 initializeLeaf :: FitchCharacterDecoration c -> FitchCharacterDecoration c
 initializeLeaf curDecoration =
@@ -110,38 +105,3 @@ updateDirVector parentDecoration parentMins childDecoration = returnChar
                         then parentDecoration ^. leftChildMin
                         else parentDecoration ^. rightChildMin
         returnChar  = curDecoration ^. discreteCharacter = median
-
-
--- | Take in a single character state as an Int, which represents an possible unambiguous character state on the parent,
--- and two decorations: the decorations of the two child states.
--- Return the minimum costs of transitioning from each of those two child decorations to the given character.
--- These mins will be saved for use at the next post-order call, to the current parent node's parent.
---
--- Note: We can throw away the medians that come back from the tcm here because we're building medians: the possible character is looped over
--- all available characters, and there's an outer loop which sends in each possible character.
-calcCostPerState :: Int -> DiscreteCharacterDecoration c -> DiscreteCharacterDecoration -> (Int, Int)
-calcCostPerState inputCharState leftChildDec rightChildDec =
-    -- Using keys, fold over alphabet states as Ints. The zipped lists will give minimum accumulated costs for each character state in each child.
-    foldlWithKey' (\(leftMin, rightMin) childCharState (accumulatedLeftCharCost, accumulatedRightCharCost) ->
-                     leftMin  = if curLeftMin < leftMin
-                                then curLeftMin
-                                else leftMin
-                     rightMin = if curRightMin < rightMin
-                                then curRightMin
-                                else rightMin
-                     where
-                         curLeftMin          = leftTransitionCost  + accumulatedLeftCharCost
-                         curRightMin         = rightTransitionCost + accumulatedRightCharCost
-                         leftTransitionCost  = (leftChildDec  ^. characterSymbolTransitionCostMatrixGenerator) inputCharState childCharState
-                         rightTransitionCost = (rightChildDec ^. characterSymbolTransitionCostMatrixGenerator) inputCharState childCharState
-                 ) (maxBound :: Word32, maxBound :: Word32) zip (leftChildDec ^. minCostVector) (rightChildDec ^. minCostVector)
-
-calcCostAndMedian :: DiscreteCharacterDecoration c -> DiscreteCharacterDecoration c -> (DiscreteCharacterDecoration c, Int)
-calcCostAndMedian leftChildDec rightChildDec = returnVal
-    where
-        returnVal = foldlWithKey' f initializedAcc $ leftChildDec ^. characterAlphabet
-        initializedAcc = ((leftChildDec ^. discreteCharacter) `xor` (leftChildDec ^. discreteCharacter), True)
-        f (outputChar, isCost) key
-            | (leftChildDec ^. discreteCharacter) `testBit` key & (rightChildDec ^. discreteCharacter) `testBit` key =
-                (outputChar `setBit` key, False)
-
