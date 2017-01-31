9f7876a9ba4060d5220b233f42a4975c9522cd7b
Alex Washburn <awashburn@amnh.org>
2017-01-31 08:54:27 -0500

    Adding Ukkonen stub module.

---
+++ production/lib/Analysis/Parsimony/Dynamic/DirectOptimization/Ukkonen.hs
@@ -0,0 +1,21 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Analysis.Parsimony.Dynamic.DirectOptimization.Pairwise
+-- Copyright   :  (c) 2015-2015 Ward Wheeler
+-- License     :  BSD-style
+--
+-- Maintainer  :  wheeler@amnh.org
+-- Stability   :  provisional
+-- Portability :  portable
+--
+-- Direct optimization export
+--
+-----------------------------------------------------------------------------
+module Analysis.Parsimony.Dynamic.DirectOptimization.Pairwise
+    ( doAlignment
+    , getOverlap
+    , minimalChoice
+    , naiveDO
+    ) where
+
+import Analysis.Parsimony.Dynamic.DirectOptimization.Pairwise.Internal
