547e7a8dc3f16681e0aca0280ba2c5147e84a780
Alex Washburn <awashburn@amnh.org>
2017-03-17 18:24:31 -0400

    Correcting Range implementation.

--- production/lib/Data/Range.hs
+++ production/lib/Data/Range.hs
@@ -22,6 +22,7 @@ module Data.Range
   , fromTuple
   , lowerBound
   , upperBound
+  , unitRange
   -- * Set-like operations on Range
   , intersects
   , intersection
@@ -35,7 +36,7 @@ module Data.Range
 
 newtype Range r = Range (r,r)
 
-type family Bound (f :: * -> *)
+type family Bound (f :: *)
 
 class Ranged a where
 
@@ -43,8 +44,9 @@ class Ranged a where
 
     fromRange :: Range (Bound a) -> a -> a
 
-    unitRange :: Bound a -> Range (Bound a)
-    unitRange x = fromTuple (x,x)
+
+unitRange :: r -> Range r
+unitRange x = fromTuple (x, x)
 
 
 fromTuple :: (r, r) -> Range r
@@ -62,8 +64,8 @@ upperBound (Range (_, rhs)) = rhs
 -- |
 -- Finds the intersection of two intervals, the intersection being the smallest interval possible. Does
 -- not assume there's an overlap.
-intersection :: Range r -> Range r -> Range r
-intersection lhs rhs = Range newLowerBound newUpperBound
+intersection :: Ord r => Range r -> Range r -> Range r
+intersection lhs rhs = Range (newLowerBound, newUpperBound)
     where
        newLowerBound = max (lowerBound lhs) (lowerBound rhs)
        newUpperBound = min (upperBound lhs) (upperBound rhs)
@@ -74,8 +76,8 @@ intersection lhs rhs = Range newLowerBound newUpperBound
 -- value to the largest possible, considering the values in both intervals.
 --
 -- Works for overlapped or subsetted intervals, as well as non-overlapping intervals.
-union :: Range r -> Range r -> Range r
-union lhs rhs = Range newLowerBound newUpperBound
+union :: Ord r => Range r -> Range r -> Range r
+union lhs rhs = Range (newLowerBound, newUpperBound)
     where
         newLowerBound = min (lowerBound lhs) (lowerBound rhs)
         newUpperBound = max (upperBound lhs) (upperBound rhs)
@@ -84,8 +86,8 @@ union lhs rhs = Range newLowerBound newUpperBound
 -- |
 -- The smallest closed interval is the smallext interval between two non-overlapping intervals, so the
 -- largest value in the leftmost interval on the number line to the smallest value of the rightmost.
-smallestClosed :: Range r -> Range r -> Range r
-smallestClosed lhs rhs = Range newLowerBound newUpperBound
+smallestClosed :: Ord r => Range r -> Range r -> Range r
+smallestClosed lhs rhs = Range (newLowerBound, newUpperBound)
     where
         newLowerBound = min (upperBound lhs) (upperBound rhs)
         newUpperBound = max (lowerBound lhs) (lowerBound rhs)
@@ -94,8 +96,8 @@ smallestClosed lhs rhs = Range newLowerBound newUpperBound
 -- |
 -- The largest closed interval between the single value on the left and the interval on the right.
 -- This is the equivalent of 'union', but works for a single value on the left, rather than an interval.
-largestClosed :: r -> Range r -> Range r
-largestClosed value interval = Range newLowerBound newUpperBound
+largestClosed :: Ord r => r -> Range r -> Range r
+largestClosed value interval = Range (newLowerBound, newUpperBound)
     where
         newLowerBound = min value $ upperBound interval
         newUpperBound = max value $ lowerBound interval
@@ -105,7 +107,7 @@ largestClosed value interval = Range newLowerBound newUpperBound
 -- The closest state is the closest value in the left interval to the right interval.
 -- This assumes that there is no overlap between the intervals. If the two intervals intersect.
 -- incorrect results will be returned.
-closestState :: Range r -> Range r -> r
+closestState :: Ord r => Range r -> Range r -> r
 closestState lhs rhs
     | upperBound lhs < lowerBound rhs = upperBound lhs
     | otherwise                       = lowerBound lhs
@@ -113,6 +115,6 @@ closestState lhs rhs
 
 -- |
 -- True if there is any overlap between the two intervals.
-intersects :: Range r -> Range r -> Bool
+intersects :: Ord r => Range r -> Range r -> Bool
 intersects lhs rhs = lowerBound rhs <= upperBound lhs
                   && upperBound rhs >= lowerBound lhs
