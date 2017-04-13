44c53dce9048793608b2ca3ec9676a5dc1113734
Alex Washburn <github@recursion.ninja>
2017-04-13 14:37:01 -0400

    Adding Semigroup instances to Evaluation Monads.

--- production/lib/Control/Evaluation/Trans.hs
+++ production/lib/Control/Evaluation/Trans.hs
@@ -11,17 +11,21 @@
 -- The 'Evaluation' type's monad transformer definition and types.
 --
 ----------------------------------------------------------------------------- 
+
 {-# LANGUAGE FlexibleInstances, MultiParamTypeClasses #-}
+
 module Control.Evaluation.Trans where
 
 import Control.Applicative
 import Control.Evaluation.Internal
 import Control.Evaluation.Unit
-import Control.Monad          (MonadPlus(mzero, mplus), join, liftM2)
+import Control.Monad               (MonadPlus(mzero, mplus), join, liftM2)
 import Control.Monad.IO.Class
 import Control.Monad.Logger
 import Control.Monad.Trans.Class
-import Data.Monoid
+import Data.Monoid          hiding ((<>))
+import Data.Semigroup
+
 
 -- | A monad transformer of 'Evaluation'.
 newtype EvaluationT m a
@@ -30,64 +34,101 @@ newtype EvaluationT m a
         runEvaluation :: m (Evaluation a)
       } 
 
--- | (✔)
-instance Functor m => Functor (EvaluationT m) where
-  fmap f x = EvaluationT . fmap (fmap f) $ runEvaluation x
-
--- | (✔)
-instance Applicative m => Applicative (EvaluationT m) where
-  pure = EvaluationT . pure . pure
-  f <*> x = EvaluationT $ liftA2 (<*>) (runEvaluation f) (runEvaluation x)
-  
--- | (✔)
-instance Monad m => Monad (EvaluationT m) where
-  return  = pure
-  fail    = EvaluationT . pure . fail
-  x >>  y = EvaluationT $ liftM2 (>>) (runEvaluation x) (runEvaluation y)
-  x >>= f = EvaluationT $ do
-              y <- runEvaluation x
-              case y of
-                Evaluation ns  NoOp     -> pure . Evaluation ns $ NoOp
-                Evaluation ns (Error e) -> pure . Evaluation ns $ Error e
-                Evaluation ns (Value v) -> (`prependNotifications` ns) <$> runEvaluation (f v)
-
--- | (✔)
-instance MonadIO m => MonadIO (EvaluationT m) where
-  liftIO = lift . liftIO
-
--- | (✔)
-instance Monad m => MonadPlus (EvaluationT m) where
-    mzero = mempty
-    mplus = (<>)
-
--- | (✔)
-instance MonadTrans EvaluationT where
-  lift = EvaluationT . fmap pure
-
--- | (✔)
-instance Monad m => Monoid (EvaluationT m a) where
-    mempty = EvaluationT $ pure mempty
-    mappend x y = EvaluationT $ liftM2 (<>) (runEvaluation x) (runEvaluation y)
 
 -- | (✔)
 instance Monad m => Alternative (EvaluationT m) where
+
     empty = mempty
+
     (<|>) x y = EvaluationT $ liftM2 (<|>) (runEvaluation x) (runEvaluation y)
+
+
+-- | (✔)
+instance Applicative m => Applicative (EvaluationT m) where
+
+    pure = EvaluationT . pure . pure
+
+    f <*> x = EvaluationT $ liftA2 (<*>) (runEvaluation f) (runEvaluation x)
+  
+
+-- | (✔)
+instance Functor m => Functor (EvaluationT m) where
+
+    fmap f x = EvaluationT . fmap (fmap f) $ runEvaluation x
+
 
 -- | (✔)
 instance Monad m => Logger (EvaluationT m) a where
-  info = state . info
-  warn = state . warn
 
--- | Takes an 'IO' value and lifts it into the evaluation context.
+    info = state . info
+
+    warn = state . warn
+
+
+-- | (✔)
+instance Monad m => Monad (EvaluationT m) where
+
+    return  = pure
+
+    fail    = EvaluationT . pure . fail
+
+    x >>  y = EvaluationT $ liftM2 (>>) (runEvaluation x) (runEvaluation y)
+    x >>= f = EvaluationT $ do
+                y <- runEvaluation x
+                case y of
+                  Evaluation ns  NoOp     -> pure . Evaluation ns $ NoOp
+                  Evaluation ns (Error e) -> pure . Evaluation ns $ Error e
+                  Evaluation ns (Value v) -> (`prependNotifications` ns) <$> runEvaluation (f v)
+
+
+-- | (✔)
+instance Monad m => MonadPlus (EvaluationT m) where
+
+    mzero = mempty
+
+    mplus = (<>)
+
+
+-- | (✔)
+instance MonadIO m => MonadIO (EvaluationT m) where
+
+    liftIO = lift . liftIO
+
+
+-- | (✔)
+instance MonadTrans EvaluationT where
+
+    lift = EvaluationT . fmap pure
+
+
+-- | (✔)
+instance Monad m => Monoid (EvaluationT m a) where
+
+    mempty  = EvaluationT $ pure mempty
+
+    mappend = (<>)
+
+
+-- | (✔)
+instance Monad m => Semigroup (EvaluationT m a) where
+
+    x <> y = EvaluationT $ liftM2 (<>) (runEvaluation x) (runEvaluation y)
+
+
+-- |
+-- Takes an 'IO' value and lifts it into the evaluation context.
 impure :: IO a -> EvaluationT IO a
 impure = liftIO
 
--- | Takes an 'Evaluation' and lifts it into the transformer context.
+
+-- |
+-- Takes an 'Evaluation' and lifts it into the transformer context.
 state :: Monad m => Evaluation a -> EvaluationT m a
 state = EvaluationT . pure 
 
--- | Prints an 'IO' parameterized transformer of 'Evaluation' context to
---   the STDOUT.
+
+-- |
+-- Prints an 'IO' parameterized transformer of 'Evaluation' context to
+-- the STDOUT.
 showRun :: Show a => EvaluationT IO a -> IO ()
 showRun = join . fmap print . runEvaluation
--- production/lib/Control/Evaluation/Unit.hs
+++ production/lib/Control/Evaluation/Unit.hs
@@ -10,11 +10,13 @@
 --
 -- The core monoidal state of an 'Evaluation' monad.
 -----------------------------------------------------------------------------
+
 module Control.Evaluation.Unit where
 
 import Control.Applicative
-import Control.Monad (MonadPlus(mzero, mplus))
-import Data.Monoid
+import Control.Monad      (MonadPlus(mzero, mplus))
+import Data.Monoid hiding ((<>))
+import Data.Semigroup
 import Test.QuickCheck
 
 
@@ -26,50 +28,78 @@ data EvalUnit a
    | Value a
    deriving (Eq,Show)
 
--- | (✔)
-instance Arbitrary a => Arbitrary (EvalUnit a) where
-  arbitrary = oneof [pure mempty, pure $ fail "Error Description", pure <$> arbitrary]
-
--- | (✔)
-instance Functor EvalUnit where
-  _ `fmap` NoOp    = NoOp
-  _ `fmap` Error x = Error x
-  f `fmap` Value x = Value $ f x
-
--- | (✔)
-instance Applicative EvalUnit where
-  pure = Value
-  NoOp    <*> _ = NoOp
-  Error x <*> _ = Error x
-  Value f <*> x = f <$> x
-
--- | (✔)
-instance Monad EvalUnit where
-  return = pure
-  fail   = Error
-  Error x >>  _ = Error x
-  _       >>  e = e
-  NoOp    >>= _ = NoOp
-  Error x >>= _ = Error x
-  Value x >>= f = f x
-
--- | (✔)
-instance MonadPlus EvalUnit where
-  mzero = mempty
-  mplus = (<>)
-
--- | (✔)
-instance Monoid (EvalUnit a) where
-  mempty  = NoOp
-  NoOp    `mappend` e    = e
-  e       `mappend` NoOp = e
-  Error x `mappend` _    = Error x
-  Value _ `mappend` e    = e
 
 -- | (✔)
 instance Alternative EvalUnit where
-  empty = mempty
-  Value x <|> _    = Value x
-  e       <|> NoOp = e
-  _       <|> e    = e
+
+    empty = mempty
+
+    Value x <|> _    = Value x
+    e       <|> NoOp = e
+    _       <|> e    = e
+
+
+-- | (✔)
+instance Applicative EvalUnit where
+
+    pure = Value
+
+    NoOp    <*> _ = NoOp
+    Error x <*> _ = Error x
+    Value f <*> x = f <$> x
+
+
+-- | (✔)
+instance Arbitrary a => Arbitrary (EvalUnit a) where
+
+    arbitrary = oneof [pure mempty, pure $ fail "Error Description", pure <$> arbitrary]
+
+
+-- | (✔)
+instance Functor EvalUnit where
+
+    _ `fmap` NoOp    = NoOp
+    _ `fmap` Error x = Error x
+    f `fmap` Value x = Value $ f x
+
+
+-- | (✔)
+instance Monad EvalUnit where
+
+    return = pure
+
+    fail   = Error
+
+    Error x >>  _ = Error x
+    _       >>  e = e
+
+    NoOp    >>= _ = NoOp
+    Error x >>= _ = Error x
+    Value x >>= f = f x
+
+
+-- | (✔)
+instance MonadPlus EvalUnit where
+
+    mzero = mempty
+
+    mplus = (<>)
+
+
+-- | (✔)
+instance Monoid (EvalUnit a) where
+
+    mempty  = NoOp
+
+    mappend = (<>)
+
+
+-- | (✔)
+instance Semigroup (EvalUnit a) where
+
+    NoOp    <> e    = e
+    e       <> NoOp = e
+    Error x <> _    = Error x
+    Value _ <> e    = e
+
 
