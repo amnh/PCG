ad24cdf02fcb182f2f0e56b7de3dbf946c422ce9
Alex Washburn <github@recursion.ninja>
2017-04-13 22:41:55 -0400

    Adding extraction of minimal character blocks and display tree edge sets.

--- production/lib/Bio/PhyloGraphPrime/PhylogeneticDAG/NetworkEdgeQuantification.hs
+++ production/lib/Bio/PhyloGraphPrime/PhylogeneticDAG/NetworkEdgeQuantification.hs
@@ -16,13 +16,16 @@
 
 module Bio.PhyloGraphPrime.PhylogeneticDAG.NetworkEdgeQuantification where
 
-import           Bio.Sequence.Block       (CharacterBlock)
+import           Bio.Character.Decoration.Shared
+import           Bio.Metadata.General
+import           Bio.Sequence
 import           Bio.PhyloGraphPrime.EdgeSet
 import           Bio.PhyloGraphPrime.Node
 import           Bio.PhyloGraphPrime.PhylogeneticDAG.Internal
 import           Bio.PhyloGraphPrime.ReferenceDAG.Internal
+import           Control.Arrow            ((&&&))
 import           Data.Key
-import           Data.List.NonEmpty       (NonEmpty)
+import           Data.List.NonEmpty       (NonEmpty((:|)))
 import qualified Data.List.NonEmpty as NE
 import           Data.List.Utility
 import           Data.Ord
@@ -47,9 +50,33 @@ extractNetworkEdgeSet :: PhylogeneticDAG2 e n u v w x y z -> EdgeSet (Int, Int)
 extractNetworkEdgeSet (PDAG2 dag) = getEdges dag
 
 
-extractBlocksMinimalEdgeSets :: PhylogeneticDAG2 e n u v w x y z -> NonEmpty (CharacterBlock u v w x y z, NonEmpty (NetworkDisplayEdgeSet (Int,Int)))
-extractBlocksMinimalEdgeSets (PDAG2 dag) = undefined
+extractBlocksMinimalEdgeSets ::
+  ( HasCharacterCost   u Word
+  , HasCharacterCost   v Word
+  , HasCharacterCost   w Double
+  , HasCharacterCost   x Word
+  , HasCharacterCost   y Word
+  , HasCharacterCost   z Word
+  , HasCharacterWeight u Double
+  , HasCharacterWeight v Double
+  , HasCharacterWeight w Double
+  , HasCharacterWeight x Double
+  , HasCharacterWeight y Double
+  , HasCharacterWeight z Double
+  ) => PhylogeneticDAG2 e n u v w x y z -> NonEmpty (Double, NonEmpty (NetworkDisplayEdgeSet (Int,Int)))
+extractBlocksMinimalEdgeSets (PDAG2 dag) = foldMapWithKey1 f sequenceBlocksWLOG
   where
-    roots = resolutions . nodeDecoration . (refs !) <$> rootRefs dag
+    generateBlockCosts = fmap (fmap (fmap (fmap blockCost . toBlocks)))
+    rootingResolutions = generateBlockCosts $ pairwiseSequence resolutionsDoNotOverlap rootResolutions
+    sequenceBlocksWLOG = toBlocks . characterSequence . NE.head $ NE.head rootResolutions
+    rootResolutions = resolutions . nodeDecoration . (refs !) <$> rootRefs dag
     refs  = references dag
-    
+
+    focusOnBlockIndex i = (sum . fmap ((! i) . characterSequence)) &&& (fromEdgeSets . NE.fromList . fmap subtreeEdgeSet)
+
+    f k _v =
+        case minimaValues of
+          x:xs -> pure (fst x, fmap snd $ x:|xs) 
+          []   -> error ""
+      where
+        minimaValues = minimaBy (comparing fst) $ focusOnBlockIndex k <$> rootingResolutions
