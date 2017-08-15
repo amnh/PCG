17e6c81b3fca9fb39582416562249e227ba59d61
Eric Ford <eric@twistcollective.com>
2017-08-15 11:11:16 -0400

    Added Text/XML.hs.

---
+++ production/lib/Text/XML.hs
@@ -0,0 +1,29 @@
+---------------------------------------------------------------------------
+-- |
+-- Module      :  Text.XML
+-- Copyright   :  (c) 2015-2015 Ward Wheeler
+-- License     :  BSD-style
+--
+-- Maintainer  :  wheeler@amnh.org
+-- Stability   :  provisional
+-- Portability :  portable
+--
+-- Output tree structure and node decoration data to XML.
+--
+-----------------------------------------------------------------------------
+
+
+module Text.XML
+    ( ToXML (..)
+    , Content (CRef)
+    , collapseElemList
+    , ppTopElement
+    , xmlAttr
+    , xmlContent
+    , xmlElement
+    , xmlQName
+    ) where
+
+
+import Text.XML.Class
+import Text.XML.Light
