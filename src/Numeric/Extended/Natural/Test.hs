860e5a8482d0b018175d60ffff9a2e92655d7d02
Alex Washburn <github@recursion.ninja>
2017-11-15 10:14:34 -0500

    Correcting TNT test suite.

--- src/Numeric/Extended/Natural/Test.hs
+++ src/Numeric/Extended/Natural/Test.hs
@@ -22,19 +22,24 @@ testSuite = testGroup "ExtendedNatural tests"
 testInvariants :: TestTree
 testInvariants = testGroup "Invariant corner cases"
     [ maxBoundCases
+    , minBoundCases
     ]
 
 
 maxBoundCases :: TestTree
 maxBoundCases = testGroup "'maxBound' specific cases"
-    [ testCase "(maxBound :: ExtendedNatural) ==  (maxBound :: Word) - 1" definitionalValue
+    [ testCase "(maxBound :: ExtendedNatural) == fromFinite (maxBound :: Word) - 1" injectiveDefinitionalValue
+    , testCase "unsafeToFinite (maxBound :: ExtendedNatural) == (maxBound :: Word) - 1" surjectiveDefinitionalValue
     , testCase "succ function does not increment maxBound" successorCase
-    , testCase "pred function decraments maxBound" predecessorCase
+    , testCase "pred function decrements maxBound" predecessorCase
     , testCase "Multiplicative identity holds" multiplicativeIdentityCase
     ]
   where
-    definitionalValue =
+    injectiveDefinitionalValue =
       (maxBound :: ExtendedNatural) @?= fromFinite ((maxBound :: Word) - 1)
+
+    surjectiveDefinitionalValue =
+      unsafeToFinite (maxBound :: ExtendedNatural) @?= (maxBound :: Word) - 1
 
     successorCase =
       succ (maxBound :: ExtendedNatural) @?= (maxBound :: ExtendedNatural)
@@ -44,3 +49,24 @@ maxBoundCases = testGroup "'maxBound' specific cases"
 
     multiplicativeIdentityCase =
       (maxBound :: ExtendedNatural) * 1 @?= (maxBound :: ExtendedNatural)
+
+
+minBoundCases :: TestTree
+minBoundCases = testGroup "'minBound' specific cases"
+    [ testCase "(minBound :: ExtendedNatural) == fromFinite (minBound :: Word)" injectiveDefinitionalValue
+    , testCase "unsafeToFinite (minBound :: ExtendedNatural) == (minBound :: Word)" surjectiveDefinitionalValue
+    , testCase "succ function increment minBound" successorCase
+    , testCase "pred function does not decrements minBound" predecessorCase
+    ]
+  where
+    injectiveDefinitionalValue =
+      (minBound :: ExtendedNatural) @?= fromFinite (minBound :: Word)
+
+    surjectiveDefinitionalValue =
+      unsafeToFinite (minBound :: ExtendedNatural) @?= (minBound :: Word)
+
+    successorCase =
+      succ (maxBound :: ExtendedNatural) @?= (maxBound :: ExtendedNatural) + 1
+
+    predecessorCase =
+      pred (maxBound :: ExtendedNatural) @?= (maxBound :: ExtendedNatural)
