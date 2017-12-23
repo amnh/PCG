066a13ea549b09bfb7fd46183f350712c6fd80c0
Alex Washburn <github@recursion.ninja>
2017-12-23 11:55:53 -0500

    Starting a benchmark set for BitVector.

--- lib/pcg-utility/Benchmarks.hs
+++ lib/pcg-utility/Benchmarks.hs
@@ -2,12 +2,14 @@ module Main (main) where
 
 
 import           Criterion.Main
+import qualified Data.BitVector.Bench           as BitVector
 import qualified Data.MutualExclusionSet.Bench  as MutualExclusionSet
 import qualified Numeric.Extended.Natural.Bench as ExtendedNatural
 
 
 main :: IO ()
 main = defaultMain
-    [ MutualExclusionSet.benchmarks
-    , ExtendedNatural.benchmarks
+    [ BitVector.benchmarks
+--    , MutualExclusionSet.benchmarks
+--    , ExtendedNatural.benchmarks
     ]
