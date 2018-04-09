64665a8297ea247f05ef6326f7c2527f50f18f35
Alex Washburn <github@recursion.ninja>
2018-04-09 13:58:49 -0400

    Adding IsString instance and 'unlabeled' definition.

--- src/Data/NodeLabel.hs
+++ src/Data/NodeLabel.hs
@@ -12,17 +12,17 @@
 --
 -----------------------------------------------------------------------------
 
-{-# LANGUAGE DeriveFoldable, DeriveGeneric, GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DeriveGeneric, FlexibleContexts, GeneralizedNewtypeDeriving, TypeFamilies #-}
 
 module Data.NodeLabel
   ( NodeLabel()
-  , fromDouble
-  , fromDoubleMay
+  , unlabeled
   ) where
 
 
 import Control.DeepSeq
-import Data.Foldable
+import Data.MonoTraversable
+import Data.String
 import GHC.Generics
 
 
@@ -30,16 +30,32 @@ import GHC.Generics
 -- A node edge annotation for representing a possibly present user supplied
 -- label.
 --
+-- Use 'IsString' instance to construct a user supplied 'NodeLabel'.
+--
+-- Use 'unlabeled' to create the empty label.
+--
 -- The primary use of 'NodeLabel' is for well-typed rendering. Use the 'Show'
 -- instance to nice render the 'NodeLabel'. If the user specified a label for
 -- the node, that string will be returned from the call to 'show'. If the user
 -- did not specify a label for the node, the Show insatnce will return
 -- @{Unlabeled Node}@.
 newtype NodeLabel = NL String
-    deriving(Eq, Foldable, Generic, NFData, Ord)
+    deriving(Eq, Generic, MonoFoldable, NFData, Ord)
+
+
+type instance Element NodeLabel = Char
+
+
+instance IsString NodeLabel where
+
+    fromString = NL
 
 
 instance Show NodeLabel where
 
     show (NL []) = "{Unlabeled Node}"
     show (NL xs) = xs
+
+
+unlabeled :: NodeLabel
+unlabeled = NL []
