24ef8886e34d2ff3361a08f5ff8596cc2291463f
Alex Washburn <github@recursion.ninja>
2018-06-08 10:19:54 -0400

    Updating resolver to be a bit clearer. Removing dead code.

--- utils/newick-resolver.hs
+++ utils/newick-resolver.hs
@@ -34,15 +34,15 @@ main = do
             inputStream  <- readFile inputfileName
             case parse' newickStreamParser inputfileName inputStream of
                 Left  errMsg -> putStrLn $ parseErrorPretty' inputfileName errMsg
-                -- Right forest -> print $ fmap resolveNode <$> forest
-                -- Right forest -> mapM_ putStrLn . sconcat $ fmap (renderNewickForest . resolveNode) <$> forest
-                Right forest -> mapM_ putStrLn . sconcat $ fmap (foldMap (<>"\n") . fmap renderBinaryTree . unfoldr descendants) <$> forest
+                Right forest -> nicelyPrintAllResolutions $ renderAllResolutions <$> forest
     where
         parse' :: Parsec Void s a -> String -> s -> Either (ParseError (Token s) Void) a
         parse' = parse
         appendSemicolon = (<> ";")
         getLabel = fromJust . newickLabel
         renderBinaryTree = appendSemicolon . renderNewickString . fmap getLabel
+        renderAllResolutions = fmap (foldMap (<>"\n") . fmap renderBinaryTree . unfoldr descendants)
+        nicelyPrintAllResolutions = mapM_ putStrLn . sconcat
 
 
 -- |
@@ -117,56 +117,3 @@ splittings (x:|[y])  = pure (pure x, pure y)
 splittings (x:|y:ys) = pure (pure x, y:|ys) <> do
     e@(lhs, rhs) <- splittings $ y:|ys
     (pure x <> lhs, rhs) :| [(lhs, pure x <> rhs)]
-
-
-{-
--- |
--- Given a node of a tree, return a non-empty set of all the unique
--- ways that node's subtrees can be resolved as binary trees.
-resolveNode :: NewickNode -> NewickForest
-resolveNode node =
-    case descendants node of
-      []   -> pure node
-      [x]  -> resolveNode x >>= (\y -> resolveNode $ makeNode [y])
-           -- This was non-obvious. Subtrees are Nodes themselves,
-           -- so must be unpacked.
-      x:xs -> do
-          (lhs, rhs)  <- splittings $ x:|xs
-          lhsJoined   <- joinSubtrees lhs
-          rhsJoined   <- joinSubtrees rhs
-          lhsResolved <- resolveNode lhsJoined
-          rhsResolved <- resolveNode rhsJoined
-          pure $ makeNode [lhsResolved, rhsResolved]
-
-
--- |
--- Given a non-empty set of elements, returns all the unique ways
--- to split the input set into  two non-empty sets
-splittings :: NonEmpty a -> NonEmpty (NonEmpty a, NonEmpty a)
-splittings (_:|[])   = error "Singleton list of chilren supplied"
-splittings (x:|[y])  = pure (pure x, pure y)
-splittings (x:|y:ys) = pure (pure x, y:|ys) <> do
-    (lhs, rhs) <- splittings $ y:|ys
-    (pure x <> lhs, rhs) :| [(lhs, pure x <> rhs)]
-
-
--- |
--- Given a non-empty set of subtrees, this function returns a list of
--- all the ways to join the subtrees together into a single tree.
---
--- Note that the newly created internal vertices are decorated with
--- the 'mempty' value of the monoidal decoration type.
-joinSubtrees :: NewickForest -> NewickForest
-joinSubtrees e@(_:|[]) = e
-joinSubtrees subtrees = do
-    (lhs, rhs)  <- splittings subtrees
-    lhsResolved <- joinSubtrees lhs
-    rhsResolved <- joinSubtrees rhs
-    resolveNode $ makeNode [lhsResolved, rhsResolved]
-
-
--- |
--- Wrapper for a smart constructor, just to shorten the code.
-makeNode :: [NewickNode] -> NewickNode
-makeNode children = fromJust $ newickNode children Nothing Nothing
--}
