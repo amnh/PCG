faac9b225db843ab1b81120fd6ae86f4b3bdf879
Callan McGill <callan.mcgill@gmail.com>
2018-07-19 12:08:25 -0400

    Add outline for list tests

---
+++ lib/pcg-utility/test/Data/List/Test.hs
@@ -0,0 +1,9 @@
+module Data.List.Test
+  ( testSuite
+  ) where
+
+import Data.List
+import Test.Tasty
+import qualified Test.Tasty.Hunit as HU
+import qualified  Test.Tasty.QuickCheck QC
+import qualified  Test.Tasty.SmallCheck as SC
