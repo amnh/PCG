43091b91ec69206815187137bc5d531661eaeff4
Boarders <callan.mcgill@gmail.com>
2018-09-10 16:50:03 -0400

    Add a variant of fold which is more strict in the accumulator

--- lib/pcg-utility/src/Data/Foldable/Custom.hs
+++ lib/pcg-utility/src/Data/Foldable/Custom.hs
@@ -8,16 +8,28 @@
 -- Stability   :  provisional
 -- Portability :  portable
 --
--- This adds strict version of foldMap.
--- Note: This is due to be added to Foldable in base 4.13.
+-- This adds strict version of some functions from Foldable.
+-- Note: FoldMap' is due to be added to Foldable in base 4.13.
 --
 -----------------------------------------------------------------------------
 
 module Data.Foldable.Custom where
 
+import Data.Coerce   (Coercible, coerce)
 import Data.Foldable (Foldable (foldl'))
+import Data.Monoid   (Sum (..))
 
 -- | Peforms a foldMap that is strict in the accumulator.
 --
 foldMap' :: (Monoid m, Foldable f) => (a -> m) -> f a -> m
 foldMap' f = foldl' (\ acc a -> acc <> f a) mempty
+
+-- | Performs a sum that is strict in the accumulator.
+--
+sum' :: (Num a, Foldable t) => t a -> a
+sum' = getSum #. foldMap' Sum
+
+-- | There is an explanation in 'base' for why this is needed.
+(#.) :: Coercible b c => (b -> c) -> (a -> b) -> (a -> c)
+(#.) _f = coerce
+{-# INLINE (#.) #-}
