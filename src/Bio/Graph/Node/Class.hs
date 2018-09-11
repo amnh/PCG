1db0aa3698257e3005ee42dbb233b37c594d27a5
Boarders <callan.mcgill@gmail.com>
2018-09-11 14:47:34 -0400

    Add lens typeclasses in leftover class module

--- src/Bio/Graph/Node/Class.hs
+++ src/Bio/Graph/Node/Class.hs
@@ -1,3 +1,7 @@
+{-# LANGUAGE MultiParamTypeClasses  #-}
+{-# LANGUAGE TypeSynonymInstances   #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE FlexibleInstances      #-}
 ------------------------------------------------------------------------------
 -- |
 -- Module      :  Bio.Graph.Node.Class
@@ -11,6 +15,33 @@
 -----------------------------------------------------------------------------
 
 module Bio.Graph.Node.Class where
+
+import Bio.Graph.Node.Internal (PhylogeneticNode2(..), ResolutionInformation, ResolutionCache)
+import Control.Lens.Lens (Lens', lens)
+
+-- |
+-- A 'Lens' for the 'resolutions' field.
+class HasResolutions s a | s -> a where
+  _resolutions :: Lens' s a
+{-# SPECIALISE _resolutions :: Lens' (PhylogeneticNode2 s n) (ResolutionCache n) #-}
+
+instance HasResolutions (PhylogeneticNode2 s n) (ResolutionCache s) where
+  {-# INLINE _resolutions #-}
+  _resolutions = lens resolutions (\p r -> p {resolutions = r})
+
+-- |
+-- A 'Lens' for the 'nodeDecorationDatum' field.
+-- Note: The field accessor is currently called nodeDecorationDatum2.
+class HasNodeDecorationDatum s a | s -> a where
+  _nodeDecorationDatum  :: Lens' s a
+{-# SPECIALISE _nodeDecorationDatum :: Lens' (PhylogeneticNode2 s n) (ResolutionCache n) #-}
+
+instance HasNodeDecorationDatum (PhylogeneticNode2 s n) n where
+  {-# INLINE _nodeDecorationDatum #-}
+  _nodeDecorationDatum = lens nodeDecorationDatum2 (\p n -> p {nodeDecorationDatum2 = n})
+
+
+
     -- ( EdgeSet
     -- , NewickSerialization()
     -- , PhylogeneticNode (..)
