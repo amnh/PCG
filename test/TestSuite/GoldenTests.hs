c3e893840aeea10285ad472861a1c6fbcdf4c73d
Boarders <callan.mcgill@gmail.com>
2018-09-13 14:06:24 -0400

    Fix issues with filepaths and appending test log

--- test/TestSuite/GoldenTests.hs
+++ test/TestSuite/GoldenTests.hs
@@ -9,7 +9,7 @@ import System.Directory
 import System.FilePath.Posix
 import Test.Tasty
 import Test.Tasty.Golden
-import Turtle                (shell)
+import Turtle                (shell, cd, pwd, rm, decodeString, encodeString)
 
 
 testSuite :: IO TestTree
@@ -17,12 +17,15 @@ testSuite = do
   pcgFiles <- getPCGFiles goldenDir
   let extensions = ["data", "dot", "xml"]
   let testInputs = [(pcg, ext) | pcg <- pcgFiles, ext <- extensions]
+  flip shell mempty
+    $ pack
+      $ mconcat
+        ["[ -e ", testLog, " ]", " && ", "rm ", testLog] -- deletes the previous test.log
   tests <- traverse goldenTest testInputs
   pure $
     testGroup "Golden Test Suite:"
     tests
   where
-    --TODO (CM): put correct dir here"
     goldenDir :: FilePath
     goldenDir = "./datasets/golden-tests"
 
@@ -32,7 +35,7 @@ testSuite = do
 -- golden file does not exist the test will generate it.
 goldenTest :: (FilePath, String) -> IO TestTree
 goldenTest (filePath, extension) = do
-  let testName = getFileName filePath
+  let testName = filePath
   let outputFilePath = filePath -<.> extension
   let goldenFilePath = makeGolden filePath
   pure $
@@ -51,21 +54,33 @@ goldenTest (filePath, extension) = do
 -- |
 -- Runs pcg on the file passed in.
 generateOutput :: FilePath -> IO ()
-generateOutput
-  = void
-  . flip shell mempty
-  . ("stack exec pcg -- --output test.log < " <>)
-  . pack
+generateOutput fp
+  = do
+  baseDir <- pwd
+  let testFP = pack $ (encodeString baseDir) </> testLog
+  cd $ decodeString fileDir
+  flip shell mempty $
+    mconcat
+      ["stack exec pcg"
+      , "< "
+      , pack fileName
+      , " >>"
+      , testFP
+      ]
+  cd baseDir
+  where
+    (fileDir, fileName) = splitFileName fp
+
 
 -- |
 -- Recursively gets all .pcg files in a directory.
 getPCGFiles :: FilePath -> IO [FilePath]
 getPCGFiles fp = do
   subDirs <- getSubDirs fp
-  concat <$> traverse getPCGFiles subDirs
+  concat <$> traverse getPCGFilesInDir subDirs
     where
-      getPCGFiles :: FilePath -> IO [FilePath]
-      getPCGFiles =
+      getPCGFilesInDir :: FilePath -> IO [FilePath]
+      getPCGFilesInDir =
         let filterPCG = filter $ (== ".pcg") . takeExtension
           in  (filterPCG <$>) . listDirectoryWithFilePath
 
@@ -74,8 +89,17 @@ getPCGFiles fp = do
           (filterM doesDirectoryExist =<<)
         . listDirectoryWithFilePath
 
+-- |
+-- Takes a directory filepath and returns a list of all files within
+-- the directory with full filepaths.
 listDirectoryWithFilePath :: FilePath -> IO [FilePath]
 listDirectoryWithFilePath fp
   = (fmap (fp </>) <$>)
   . listDirectory
   $ fp
+
+
+-- |
+-- Name of test log file.
+testLog :: FilePath
+testLog = "test" <.> "log"
