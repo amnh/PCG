9f56bc3e0c83bf123a15ffb0a285eb7e61f1a5e2
Boarders <callan.mcgill@gmail.com>
2018-12-11 11:11:39 -0500

    Add file streaming rendering

---
+++ lib/pcg-utility/src/Data/Render/Utility.hs
@@ -0,0 +1,47 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TypeFamilies     #-}
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Data.Render.Utility
+-- Copyright   :  (c) 2015-2018 Ward Wheeler
+-- License     :  BSD-style
+--
+-- Maintainer  :  wheeler@amnh.org
+-- Stability   :  provisional
+-- Portability :  portable
+--
+-- Utility Functions for rendering text and workinng with the TextShow typeclass
+--
+-----------------------------------------------------------------------------
+
+
+module Data.Render.Utility where
+
+import Control.Exception      (bracket)
+import Control.Monad.IO.Class (MonadIO (liftIO))
+import Data.Text              (Text)
+import Data.Text.IO           (hPutStr)
+import Pipes                  (Producer, for, runEffect, yield)
+import Prelude                hiding (writeFile)
+import System.IO              (IOMode (AppendMode), hClose, openFile)
+import TextShow
+
+
+-- |
+-- Streams text to a file in constant memory for any type with a `TextShow`
+-- instance.
+writeFileT :: TextShow text => FilePath -> text -> IO ()
+writeFileT fp text = do
+  h <- openFile fp AppendMode
+  let builder = showb text
+  runEffect $ for (builderTextProducer builder) (liftIO . hPutStr h)
+  hClose h
+
+-- |
+-- Efficiently yield text from a 'Builder' type.
+builderTextProducer :: Monad m => Builder -> Producer Text m ()
+builderTextProducer = yield . toText
+
+
+
+
