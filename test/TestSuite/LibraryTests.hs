c5459aba4ce60a5699ab43c62fd3ed176964a97b
Boarders <callan.mcgill@gmail.com>
2019-01-15 16:17:21 -0500

    Remove refactored library test

--- test/TestSuite/LibraryTests.hs
+++ test/TestSuite/LibraryTests.hs
@@ -7,7 +7,6 @@ import qualified Bio.Character.Encodable.Dynamic.Test                        as 
 import qualified Bio.Character.Encodable.Static.Test                         as StaticChar
 import qualified Bio.Graph.ReferenceDAG.Test                                 as ReferenceDAG
 import qualified Control.Evaluation.Test                                     as Evaluation
-import qualified Data.Alphabet.Test                                          as Alphabet
 import qualified Data.TCM.Test                                               as TCM
 import           Test.Tasty
 
@@ -15,7 +14,6 @@ import           Test.Tasty
 testSuite :: TestTree
 testSuite = testGroup "Library Test Suite"
     [ Evaluation.testSuite
-    , Alphabet.testSuite
     , TCM.testSuite
     , DynamicChar.testSuite
     , StaticChar.testSuite
