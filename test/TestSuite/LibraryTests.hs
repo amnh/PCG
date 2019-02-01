b36320323f565b7bd386853001b2ef942398549e
Boarders <callan.mcgill@gmail.com>
2019-02-01 11:13:09 -0500

    Fix cabal file for building tests

--- test/TestSuite/LibraryTests.hs
+++ test/TestSuite/LibraryTests.hs
@@ -5,15 +5,12 @@ module TestSuite.LibraryTests
 import qualified Bio.Character.Encodable.Dynamic.Test as DynamicChar
 import qualified Bio.Character.Encodable.Static.Test  as StaticChar
 import qualified Bio.Graph.ReferenceDAG.Test          as ReferenceDAG
-import qualified Control.Evaluation.Test              as Evaluation
 import           Test.Tasty
 
 
 testSuite :: TestTree
 testSuite = testGroup "Library Test Suite"
-    [ Evaluation.testSuite
-    , DynamicChar.testSuite
+    [ DynamicChar.testSuite
     , StaticChar.testSuite
-    , Pairwise.testSuite
     , ReferenceDAG.testSuite
     ]
