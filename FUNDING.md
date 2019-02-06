957d3d7d9378350dc0b2aa00e0c844285668ba69
Alex Washburn <github@recursion.ninja>
2019-02-06 13:35:43 -0500

    Refactoring command line option parsing and handling.

---
+++ FUNDING.md
@@ -0,0 +1,7 @@
+### Funding provided by:
+
+  * [American Museum of Natural History](https://www.amnh.org/our-research/computational-sciences)
+
+  * [DARPA SIMPLEX](https://www.darpa.mil/program/simplifying-complexity-in-scientific-discovery)
+
+  * [Kleberg Foundation](http://www.klebergfoundation.org/)
--- app/Main.hs
+++ app/Main.hs
@@ -1,56 +1,20 @@
-{-# LANGUAGE DeriveGeneric    #-}
 {-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE TemplateHaskell  #-}
 
 module Main (main) where
 
 import Control.DeepSeq
 import Control.Evaluation
-import Control.Monad                (when)
-import Data.Char                    (toUpper)
-import Data.Semigroup               ((<>))
-import Data.Version                 (showVersion)
+import Data.Char                (toUpper)
+import Data.Maybe
+import Data.Semigroup           ((<>))
 import Data.Void
-import Development.GitRev           (gitCommitCount, gitHash)
-import GHC.Generics
-import Options.Applicative          hiding (ParseError)
-import Paths_phylocomgraph          (version)
+import PCG.CommandLineOptions
 import PCG.Computation.Internal
-import PCG.Syntax                   (computationalStreamParser)
+import PCG.Syntax               (computationalStreamParser)
 import System.Environment
 import System.Exit
 import System.IO
-import Text.Megaparsec              (ParseErrorBundle, Parsec, errorBundlePretty, parse)
-import Text.PrettyPrint.ANSI.Leijen (align, indent, int, line, string, text, (<+>), (</>))
-
-
--- |
--- Valid command line options
-data  CommandLineOptions
-    = CommandLineOptions
-    { inputFile    :: FilePath
-    , outputFile   :: FilePath
-    , printVersion :: Bool
-    , printSplash  :: Bool
-    , verbosity    :: Verbosity
-    } deriving (Generic)
-
-
--- |
--- Enumeration of verbosity levels.
-data Verbosity
-   = None
-   | Errors
-   | Warnings
-   | Informational
-   | Debugging
-   deriving (Eq, Enum, Generic, Show)
-
-
-instance NFData CommandLineOptions
-
-
-instance NFData Verbosity
+import Text.Megaparsec          (ParseErrorBundle, Parsec, errorBundlePretty, parse)
 
 
 -- |
@@ -67,25 +31,24 @@ main :: IO ()
 main = do
      opts <- force <$> parseCommandLineOptions
      let  _verbosity = verbosity opts
-     when (printSplash opts) printSplashImage
-     if   printVersion opts
-     then putStrLn fullVersionInformation
-     else do
-          inputStreamMaybe <- retreiveInputStream $ inputFile opts
-          case inputStreamMaybe of
-            Left errorMessage -> putStrLn errorMessage
-            Right inputStream -> do
-                (code, outputStream) <- case parse' computationalStreamParser (inputFile opts) inputStream of
-                                          Left  err -> pure (ExitFailure 4, errorBundlePretty err)
-                                          Right val -> fmap renderSearchState . runEvaluation . evaluate $ optimizeComputation val
-                let  outputPath = outputFile opts
-                if   (toUpper <$> outputPath) == "STDOUT"
-                then hSetBuffering stdout NoBuffering >> putStrLn outputStream
-                else writeFile outputPath outputStream
-                exitWith code
+     fromMaybe (performSearch opts) $ gatherDisplayInformation opts
   where
      parse' :: Parsec Void s a -> String -> s -> Either (ParseErrorBundle s Void) a
      parse' = parse
+
+     performSearch opts = do
+       inputStreamMaybe <- retreiveInputStream $ inputFile opts
+       case inputStreamMaybe of
+         Left errorMessage -> putStrLn errorMessage
+         Right inputStream -> do
+             (code, outputStream) <- case parse' computationalStreamParser (inputFile opts) inputStream of
+                                       Left  err -> pure (ExitFailure 4, errorBundlePretty err)
+                                       Right val -> fmap renderSearchState . runEvaluation . evaluate $ optimizeComputation val
+             let  outputPath = outputFile opts
+             if   (toUpper <$> outputPath) == "STDOUT"
+             then hSetBuffering stdout NoBuffering >> putStrLn outputStream
+             else writeFile outputPath outputStream
+             exitWith code
 
 
 -- |
@@ -113,139 +76,3 @@ retreiveInputStream path
            if   null args
            then Left <$> parserHelpMessage
            else Left . ("Error: STDIN is empty\n\n" <>) <$> parserHelpMessage
-
-
--- |
--- Generates the program's help menu based on the command line options parser.
-parserHelpMessage :: IO String
-parserHelpMessage = fst . renderFailure failValue <$> getProgName
-  where
-    failValue = parserFailure (prefs showHelpOnEmpty) parserInformation ShowHelpText []
-
-
--- |
--- Command to parse the command line options.
-parseCommandLineOptions :: IO CommandLineOptions
-parseCommandLineOptions = customExecParser preferences parserInformation
-  where
-    preferences = prefs $ mconcat [showHelpOnError, showHelpOnEmpty]
-
-
--- |
--- Information regarding which command line options are valid and how they are
--- parsed and interpreted.
-parserInformation :: ParserInfo CommandLineOptions
-parserInformation = info (helper <*> commandLineOptions) description
-  where
-    commandLineOptions =
-        CommandLineOptions
-          <$> fileSpec 'i' "input"  "STDIN"  "Input PCG script file"
-          <*> fileSpec 'o' "output" "STDOUT" "Output file"
-          <*> switch  (mconcat [long "version", help "Display version number"])
-          <*> switch  (mconcat [long "splash" , help "Display splash image"])
-          <*> (validateVerbosity <$> option auto verbositySpec)
-
-    fileSpec c s d h = strOption $ mconcat
-        [ short c
-        , long  s
-        , value d
-        , help  $ mconcat [h, " (default ", d, ")"]
-        , metavar "FILE"
-        ]
-
-    verbositySpec = mconcat
-        [ short 'v'
-        , long "verbosity"
-        , value 3
-        , helpDoc verbosityHelp
-        , metavar "LEVEL"
-        ]
-
-    description = mconcat
-        [ fullDesc
-        , headerDoc . Just . string $ "  " <> softwareName <> "\n  " <> shortVersionInformation
-        ]
-
-    verbosityHelp = Just . (text "Select the verbosity level (default 3):" `op`) . indent 2 . foldl1 op $ f <$>
-        [ (0, ["Suppress all output"])
-        , (1, ["Output only errors"])
-        , (2, ["Output errors and", "warnings"])
-        , (3, ["Output errors,", "warnings,", "and runtime information"])
-        , (4, ["Output errors,", "warnings,", "runtime information,", "and debugging information"])
-        ]
-      where
-        f (i, x) = int i <+> align (g x)
-        g = foldl1 (</>) . fmap text
-        op x y = x <> line <> y
-
-
--- |
--- Name of the software package.
-softwareName :: String
-softwareName = "Phylogenetic Component Graph"
-
-
--- |
--- Brief description of the software version.
-shortVersionInformation :: String
-shortVersionInformation = "(alpha) version " <> showVersion version
-
-
--- |
--- Full escription of the software version.
---
--- Uses @TemplateHaskell@ to splice in git hash and commit count information
--- from the compilation environment.
-fullVersionInformation :: String
-fullVersionInformation = mconcat
-    [ softwareName
-    , " "
-    , shortVersionInformation
-    , " ["
-    , take 7 $(gitHash)
-    , "] ("
-    , $(gitCommitCount)
-    , " commits)"
-    ]
-
-
--- |
--- Interpret an 'Integer' as a 'Verbosity' value.
---
--- 'Integer' values in the range @[0 .. 4]@ are valid.
--- Values oput side the range defualt to @3@.
-validateVerbosity :: Integer -> Verbosity
-validateVerbosity 0 = None
-validateVerbosity 1 = Errors
-validateVerbosity 2 = Warnings
-validateVerbosity 4 = Debugging
-validateVerbosity _ = Informational
-
-
-printSplashImage :: IO ()
-printSplashImage = putStrLn $ unlines
-  [ "______ _           _                             _   _      "
-  , "| ___ \\ |         | |                           | | (_)     "
-  , "| |_/ / |__  _   _| | ___   __ _  ___ _ __   ___| |_ _  ___ "
-  , "|  __/| '_ \\| | | | |/ _ \\ / _` |/ _ \\ '_ \\ / _ \\ __| |/ __|"
-  , "| |   | | | | |_| | | (_) | (_| |  __/ | | |  __/ |_| | (__ "
-  , "\\_|   |_| |_|\\__, |_|\\___/ \\__, |\\___|_| |_|\\___|\\__|_|\\___|"
-  , "              __/ |         __/ |                           "
-  , "             |___/         |___/                            "
-  , " _____                                              _       "
-  , "/  __ \\                                            | |      "
-  , "| /  \\/ ___  _ __ ___  _ __   ___  _ __   ___ _ __ | |_     "
-  , "| |    / _ \\| '_ ` _ \\| '_ \\ / _ \\| '_ \\ / _ \\ '_ \\| __|    "
-  , "| \\__/\\ (_) | | | | | | |_) | (_) | | | |  __/ | | | |_     "
-  , " \\____/\\___/|_| |_| |_| .__/ \\___/|_| |_|\\___|_| |_|\\__|    "
-  , "                      | |                                   "
-  , "                      |_|                                   "
-  , " _____                 _                                    "
-  , "|  __ \\               | |                                   "
-  , "| |  \\/_ __ __ _ _ __ | |__  ___                            "
-  , "| | __| '__/ _` | '_ \\| '_ \\/ __|                           "
-  , "| |_\ \ | | (_| | |_) | | | \\__ \\                           "
-  , " \\____/_|  \\__,_| .__/|_| |_|___/                           "
-  , "                | |                                         "
-  , "                |_|                                         "
-  ]
