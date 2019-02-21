980a776f0872240236c228c549e0da6daed9c00f
Boarders <callan.mcgill@gmail.com>
2019-02-20 21:18:23 -0500

    Add test output rendering

--- src/Bio/Graph/ReferenceDAG/Test/Network.hs
+++ src/Bio/Graph/ReferenceDAG/Test/Network.hs
@@ -20,22 +20,27 @@ import qualified Test.Tasty.QuickCheck as QC
 import           Bio.Graph.ReferenceDAG.Internal
 import qualified Data.Set as S (toList, fromList)
 import Test.QuickCheck (forAll, Property, (===))
+import Control.Applicative ((<|>))
 
 
 
 candidateNetworkProperties :: TestTree
 candidateNetworkProperties = testGroup "Properties of candidateNetworkEdges function"
     [ QC.testProperty
-        (    "candidateNetworkEdges of branched binary tree:\n"
-          <>  renderBranchedNetwork
-          <> "              equals:\n"
-          <> "TODO"
+        ( unlines $
+          [ "Given valid networks n0 and n1, the candidateNetworkEdges of a branched binary"
+          , "      tree:"
+          , renderBranchedNetwork
+          , "      are equal to the candidate network edges of n0 plus the candidate network"
+          , "      edges of n1 plus those edges from a non-network adjacent edge in n0 or n1 to"
+          , "      a non-network edge in n1 or n0 (respectively)."
+          ]
         )
         branchedNetworkProperty
     ]
   where
     branchedNetworkProperty :: Property
-    branchedNetworkProperty = forAll generateBinaryTree' correctBranchedCandidateEdges
+    branchedNetworkProperty = forAll generateBinaryTreeWithInfo correctBranchedCandidateEdges
     
     correctBranchedCandidateEdges
       :: ( ReferenceDAG () () ()
@@ -51,24 +56,24 @@ candidateNetworkProperties = testGroup "Properties of candidateNetworkEdges func
         n1NonNetworkEdges = getNonNetworkEdges n1NetInfo
 
         newNetworkEdges =
-             _candidateNetworkEdges n0NetInfo
-          <> _candidateNetworkEdges n1NetInfo
-          <> n0n1NetworkEdges
+              _candidateNetworkEdges n0NetInfo
+           <> _candidateNetworkEdges n1NetInfo
+           <> n0n1NetworkEdges
 
         n0n1NetworkEdges  =
-          S.fromList 
-            [(n0Edge, n1Edge)
-            | n0Edge <- S.toList n0NonNetworkEdges
-            , n1Edge <- S.toList n1NonNetworkEdges
-            ]
+          S.fromList $ do
+            n0Edge <- S.toList n0NonNetworkEdges
+            n1Edge <- S.toList n1NonNetworkEdges
+            pure (n0Edge, n1Edge) <|> pure (n1Edge, n0Edge)
+ 
         
 
 
 renderBranchedNetwork :: String
 renderBranchedNetwork = unlines
-    ["         r     "
-    ,"     ┌───┴───┐ "
-    ,"     │       │ "
-    ,"     │       │ "
-    ,"    n0       n1"
+    ["                                 r     "
+    ,"                             ┌───┴───┐ "
+    ,"                             │       │ "
+    ,"                             │       │ "
+    ,"                            n0       n1"
     ]
