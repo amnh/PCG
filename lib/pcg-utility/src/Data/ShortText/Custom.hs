f173b9dead4df6c9b6981a729000fde441668026
Boarders <callan.mcgill@gmail.com>
2019-06-22 19:39:05 -0400

    Add function for making illegal UTF-8 text

--- lib/pcg-utility/src/Data/ShortText/Custom.hs
+++ lib/pcg-utility/src/Data/ShortText/Custom.hs
@@ -15,9 +15,17 @@
 
 module Data.ShortText.Custom
   ( intToShortText
+  , makeIllegalShortText
+  , convertToBytes
   ) where
 
 import Data.Text.Short (ShortText, fromString)
+import Data.ByteString.Short hiding (unpack)
+import Data.Text.Short.Unsafe (fromShortByteStringUnsafe)
+import Data.Word
+import Data.ByteString.Builder
+import Data.ByteString.Lazy (unpack)
+
 
 
 -- |
@@ -25,3 +33,19 @@ import Data.Text.Short (ShortText, fromString)
 intToShortText :: Int -> ShortText
 intToShortText = fromString . show
 
+-- |
+-- The purpose of this function is to create non-UTF-8 valid 'ShortText' which is unique for each input
+-- to be used to give temporary names which are guaranteed to be distinct from those names already in the
+-- graph.
+makeIllegalShortText :: Word64 -> ShortText
+makeIllegalShortText = fromShortByteStringUnsafe . makeNonUTFByteString
+
+-- |
+-- Creates a ByteString which is invalid UTF8
+makeNonUTFByteString :: Word64 -> ShortByteString
+                        -- 192 is not allowed as a starting code point in UTF-8
+makeNonUTFByteString = pack . (\bytes -> 192 : bytes) . convertToBytes
+
+
+convertToBytes :: Word64 -> [Word8]
+convertToBytes = unpack . toLazyByteString . word64BE
