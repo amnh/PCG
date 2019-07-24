176121ea017ed793231f04698d39f6e75d3d8ede
Boarders <callan.mcgill@gmail.com>
2019-07-24 17:20:01 -0400

    Add various lenses and improve names

--- lib/pcg-graph/src/Data/Graph/NodeContext.hs
+++ lib/pcg-graph/src/Data/Graph/NodeContext.hs
@@ -1,45 +1,115 @@
-{-# LANGUAGE MagicHash             #-}
 {-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE FlexibleContexts #-}
+
+
 module Data.Graph.NodeContext where
 
 import Data.Graph.Indices
+import Control.Lens
 
-data Two a = Single !a | Pair !a !a
+data Pair a = Pair {-# UNPACK #-} !a {-# UNPACK #-} !a
+  deriving stock (Eq, Show)
+  deriving stock Functor
+
+class HasLeft s a | s -> a where
+  _left :: Lens' s a
+
+class HasRight s a | s -> a where
+  _right :: Lens' s a
+
+instance HasLeft (Pair a) a where
+  _left = lens (\(Pair a1 a2) -> a1) (\(Pair a1 a2) a1' -> Pair a1' a2)
+
+instance HasRight (Pair a) a where
+  _right = lens (\(Pair a1 a2) -> a2) (\(Pair a1 a2) a2' -> Pair a1 a2')
 
 data NodeContext
-  = NodeContext {-# UNPACK #-} !(Two ParentIndex) {-# UNPACK #-} !(Two ChildIndex)
+  = LeafC     LeafContext
+  | RootC     RootContext
+  | NetworkC  NetworkContext
+  | InternalC InternalContext
 
-data IndexData n = IndexData
-  { indexData :: n
-  , nodeContext :: !NodeContext
+leafNodeContext :: LeafContext -> NodeContext
+leafNodeContext = LeafC
+rootNodeContext :: RootContext -> NodeContext
+rootNodeContext = RootC
+networkNodeContext :: NetworkContext -> NodeContext
+networkNodeContext = NetworkC
+internalNodeContext :: InternalContext -> NodeContext
+internalNodeContext = InternalC
+
+
+data IndexData nodeContext nodeData  = IndexData
+  { nodeData    :: nodeData
+  , nodeContext :: !nodeContext
   }
 
+class HasNodeData s t a b | s -> a, t -> b, s b -> t, t a -> s where
+  _nodeData :: Lens s t a b
 
-newtype RootContext = RootContext { childInds :: Two ChildIndex}
-data RootIndexData r = RootIndexData
-  { rootIndexData :: r
-  , rootContext   :: !RootContext
+class HasNodeContext s t a b | s -> a, t -> b, s b -> t, t a -> s where
+  _nodeContext :: Lens s t a b
+
+instance HasNodeData (IndexData nc nd) (IndexData nc nd') nd nd' where
+  _nodeData = lens nodeData (\ind nd -> ind {nodeData = nd})
+
+
+instance HasNodeContext (IndexData nc nd) (IndexData nc' nd) nc nc'  where
+  _nodeContext = lens nodeContext (\ind nc -> ind {nodeContext = nc})
+
+
+newtype RootContext = RootContext
+  { childInds :: (Either ChildIndex (Pair ChildIndex))
+  }
+type RootIndexData d = IndexData RootContext d
+
+newtype LeafContext = LeafContext
+  { parentInds :: ParentIndex
+  }
+type LeafIndexData d = IndexData LeafContext d
+
+data NetworkContext = NetworkContext
+  { parentInds  :: {-# UNPACK #-} !(Pair ParentIndex)
+  , childInds   :: {-# UNPACK #-} !ChildIndex
   }
 
-newtype LeafContext = LeafContext { parentInds :: ParentIndex }
-data LeafIndexData t = LeafIndexData
-  { leafIndexData :: t
-  , leafContext   :: !LeafContext
+type NetworkIndexData d = IndexData NetworkContext d
+
+data InternalContext = InternalContext
+  { parentInds :: {-# UNPACK #-} !ParentIndex
+  , childInds  :: {-# UNPACK #-} !(Pair ChildIndex)
   }
 
-data NetworkContext
-  = NetworkContext {-# UNPACK #-} !(Two ParentIndex) {-# UNPACK #-} !ChildIndex
+type InternalIndexData d = IndexData InternalContext d
 
-data NetworkIndexData n = NetworkIndexData
-  { networkIndexData :: n
-  , networkContext   :: !NetworkContext
-  }
+class HasParentInds s a | s -> a where
+  _parentInds :: Lens' s a
 
-data InternalContext
-  = InternalContext {-# UNPACK #-} !ParentIndex {-# UNPACK #-} !(Two ChildIndex)
+class HasChildInds s a | s -> a where
+  _childInds :: Lens' s a
 
-data InternalIndexData i = InternalIndexData
-  { internalIndexData :: i
-  , internalContext   :: !InternalContext
-  }
+instance HasParentInds LeafContext ParentIndex where
+  _parentInds = undefined
 
+instance HasParentInds InternalContext ParentIndex where
+  _parentInds = undefined
+
+instance HasParentInds NetworkContext (Pair ParentIndex) where
+  _parentInds = undefined
+
+instance HasChildInds RootContext (Either ChildIndex (Pair ChildIndex)) where
+  _childInds = undefined
+
+instance HasChildInds InternalContext (Pair ChildIndex) where
+  _childInds = undefined
+
+instance HasChildInds NetworkContext ChildIndex where
+  _childInds = undefined
+
+instance HasChildInds ctxt ChildIndex => HasChildInds (IndexData ctxt n) ChildIndex where
+  _childInds = _nodeContext . _childInds
