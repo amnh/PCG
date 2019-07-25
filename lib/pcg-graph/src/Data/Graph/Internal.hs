6abecf6e97418456111d6125a4f0ea069513c2bb
Boarders <callan.mcgill@gmail.com>
2019-07-25 16:07:58 -0400

    Add postorder function

--- lib/pcg-graph/src/Data/Graph/Internal.hs
+++ lib/pcg-graph/src/Data/Graph/Internal.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE LambdaCase     #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Data.Graph.Internal where
 
@@ -13,6 +14,8 @@ import Data.Vector.Instances ()
 import Control.Lens
 import Data.Bifunctor
 import Data.Graph.Type
+import Data.Graph.Memo
+import Control.Applicative
 
 
 postorderFold
@@ -22,3 +25,23 @@ postorderFold
   -> ((f n) -> r -> r -> r)
   -> Graph f e c n t -> r
 postorderFold = undefined
+
+
+postorder
+  :: forall g f e c n1 n2 t . (Applicative g)
+  => (t -> n2)
+  -> (n2 -> n2 -> n2)
+  -> Graph f e c n1 t
+  -> Graph g e c n2 n2
+postorder leafFn internalFn graph =
+  let
+    memoGen :: Endo (MemoGenGraph (g n2) n2)
+    memoGen = memoGraphPostorder leafFn internalFn id graph
+
+    numberL = lengthOf _leafReferences     graph
+    numberI = lengthOf _internalReferences graph
+    numberN = lengthOf _networkReferences  graph
+    numberR = lengthOf _rootReferences     graph
+    cacheInfo      = graph ^. _cachedData
+  in
+    generateMemoGraph cacheInfo numberL numberI numberN numberR memoGen
