ea5bd184e5e0b0f382eda8654b1f9b5f6b02bf86
Boarders <callan.mcgill@gmail.com>
2019-08-01 18:21:51 -0400

    Finish incremental postorder function

--- lib/pcg-graph/src/Data/Graph/Internal.hs
+++ lib/pcg-graph/src/Data/Graph/Internal.hs
@@ -2,15 +2,22 @@
 {-# LANGUAGE LambdaCase     #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE MagicHash #-}
 
 module Data.Graph.Internal where
 
 import Data.Vector.Instances ()
-import Control.Lens
+import Control.Lens hiding (index)
 import Data.Graph.Type
 import Data.Graph.Memo
+import Data.Vector (Vector)
+import qualified Data.Vector as V
+import qualified Data.Vector.Mutable as MV
+import Data.Graph.Indices
+import Data.Graph.NodeContext
+import Data.Coerce
 
 
 postorderFold
@@ -28,13 +35,13 @@ postorder
   -> (n2 -> n2 -> n2)
   -> Graph f e c n1 t
   -> Graph g e c n2 n2
-postorder leafFn internalFn graph =
+postorder leafFn treeFn graph =
   let
     memoGen :: Endo (MemoGenGraph (g n2) n2)
-    memoGen = memoGraphPostorder leafFn internalFn id graph
+    memoGen = memoGraphPostorder leafFn treeFn id graph
 
     numberL = lengthOf _leafReferences     graph
-    numberI = lengthOf _internalReferences graph
+    numberI = lengthOf _treeReferences graph
     numberN = lengthOf _networkReferences  graph
     numberR = lengthOf _rootReferences     graph
     cacheInfo      = graph ^. _cachedData
@@ -43,37 +50,56 @@ postorder leafFn internalFn graph =
 
 
 incrementalPostorder
-  :: forall g f e c n t .
-  -> Int
-  -> (n -> n -> True)
+  :: forall f e c n . Applicative f
+  => Int
+  -> (f n -> f n -> Bool)
   -> (n -> n)
   -> (n -> n -> n)
   -> Graph f e c n n
   -> Graph f e c n n
-incrementalPostorder startInd thresholdFn updateFn internalFn graph =
-    graph %~ _internalReferences
+incrementalPostorder startInd thresholdFn updateFn treeFn graph = f graph
   where
-    taggedIndex = tagValue InternalTag internalInd
+    taggedIndex :: TaggedIndex
+    taggedIndex = tagValue TreeTag startInd
 
-    startingValue :: n
-    startingValue = graph ^. _internalReferences . singular (ix i)
+    startingValue :: f n
+    startingValue = graph ^. _treeReferences . singular (ix startInd) . _nodeData
 
- -- This would be more efficient using the ST monad
-    go :: TaggedIndex -> (Maybe n, Vector (InternalIndexData (f n)))
-    go tagIndex currValue ~(val, currVect) =
+
+ -- TODO: rewrite using ST.
+    f :: Graph f e c n n -> Graph f e c n n
+    f g = case go taggedIndex (Nothing, g ^. _treeReferences) of
+      (Nothing       , v') -> g & _treeReferences .~ v'
+      (Just (r, ind) , v') -> g & _treeReferences .~ v'
+                                & _rootReferences  %~ (modifyNodeData (untagValue ind) r)
+      
+ -- TODO: rewrite using ST.      
+    go
+      :: TaggedIndex
+      -> (Maybe ((f n), ParentIndex), Vector (TreeIndexData (f n)))
+      -> (Maybe ((f n), ParentIndex), Vector (TreeIndexData (f n))) 
+    go tagInd ~(val, currVect) =
       let
-        currVal   = graph ^. _internalReferences . singular (ix i) . _nodeData
-        parInd    = graph ^. _internalReferences . singular (ix i) . _parentInds
-        childInds = graph ^. _internalReferences . singular (ix i) . _childInds
+        ind = untagValue tagInd
+        currVal   = graph ^. _treeReferences . singular (ix ind) . _nodeData
+        parInd    = graph ^. _treeReferences . singular (ix ind) . _parentInds
+        childInds :: Pair ChildIndex
+        childInds = graph ^. _treeReferences . singular (ix ind) . _childInds
+        childIndData1 = graph `index` (coerce $ childInds ^. _left)
+        childIndData2  = graph `index` (coerce $ childInds ^. _right)
         parTag    = getTag $ parInd
-        newVal    = internalFn
-                      (childInds ^. _left)
-                      (childInds ^. _right)
+        newVal    = (liftFunction treeFn) childIndData1 childIndData2
      in
-       if parTag == RootTag
-         then (Just newVal, currVect)
+       if
+         thresholdFn currVal newVal
+       then
+         (val, currVect)
+       else
+         if
+           parTag == RootTag
+         then (Just (newVal, parInd), currVect)
          else
            let updatedVect = undefined in
-             go parInd (val, updatedVect)
+             go (coerce parInd) (val, updatedVect)
         
      
