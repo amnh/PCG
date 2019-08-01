3e300b3b08e5e4ab7129a2d9203c1133b0037f61
Boarders <callan.mcgill@gmail.com>
2019-08-01 18:21:24 -0400

    Add tagged typeclass

--- lib/pcg-graph/src/Data/Graph/Indices.hs
+++ lib/pcg-graph/src/Data/Graph/Indices.hs
@@ -3,7 +3,8 @@
 {-# LANGUAGE DerivingVia                #-}
 {-# LANGUAGE DerivingStrategies         #-}
 {-# LANGUAGE DataKinds #-}
-{-# LANGUAGE MagicHash #-}
+{-# LANGUAGE InstanceSigs #-}
+
 
 module Data.Graph.Indices where
 
@@ -26,21 +27,26 @@ newtype NetworkInd = NetworkInd {getNetworkInd  :: Word64}
   deriving stock (Eq, Show)
   deriving (Semigroup, Monoid) via (Sum Word64)
 
-newtype InternalInd = InternalInd {getInternalInd :: Word64}
+newtype TreeInd = TreeInd {getTreeInd :: Word64}
   deriving stock (Eq, Show)
   deriving (Semigroup, Monoid) via (Sum Word64)
 
 
-data Ind = L LeafInd | R RootInd | N NetworkInd | I InternalInd
-data ChildInd = CL LeafInd | CN NetworkInd | CI InternalInd
-data ParentInd = PR RootInd | PN NetworkInd | PI InternalInd
+data Ind = L LeafInd | R RootInd | N NetworkInd | I TreeInd
+data ChildInd = CL LeafInd | CN NetworkInd | CI TreeInd
+data ParentInd = PR RootInd | PN NetworkInd | PI TreeInd
+
+class Tagged t where
+  tagValue   :: IndexType -> Int -> t
+  getTag     :: t -> IndexType
+  untagValue :: t -> Int
 
 
 -- Tag Info:
 --       |      Tag        |    IndexType    |
 --       |  bit63, bit62   |
 --       |     0 , 0       |    Leaf         |
---       |     1 , 0       |    Internal     |
+--       |     1 , 0       |    Tree     |
 --       |     0 , 1       |    Network      |
 --       |     1 , 1       |    Root         |
 
@@ -49,26 +55,29 @@ newtype TaggedIndex  = TaggedIndex {getIndex :: Int }
   deriving (Semigroup, Monoid) via (Sum Int)
   deriving newtype Bits
 
+instance Tagged TaggedIndex where
+  tagValue :: IndexType -> Int -> TaggedIndex
+  tagValue ty i = TaggedIndex (i .|. fromEnum ty `shiftL` 61)
+
+  getTag :: TaggedIndex -> IndexType
+  getTag = toEnum . getIndex . (`shiftR` 61)
+
+  untagValue :: TaggedIndex -> Int
+  untagValue (TaggedIndex i) = clearBit (clearBit i 62) 61
+    
+
 newtype ParentIndex  = ParentIndex {getParentIndex :: TaggedIndex}
   deriving stock (Eq, Show)
   deriving (Semigroup, Monoid) via (Sum Int)
-  deriving newtype Bits
+  deriving newtype (Bits, Tagged)
 
 newtype ChildIndex   = ChildIndex  {getChildIndex :: TaggedIndex}
   deriving stock (Eq, Show)
   deriving (Semigroup, Monoid) via (Sum Int)
-  deriving newtype (Enum, Bits)
+  deriving newtype (Bits, Tagged)
 
-data IndexType = LeafTag | InternalTag | NetworkTag | RootTag
-  deriving Enum
-
-
-tagValue :: IndexType -> Int -> TaggedIndex
-tagValue ty i = TaggedIndex (i .|. fromEnum ty `shiftL` 61)
+data IndexType = LeafTag | TreeTag | NetworkTag | RootTag
+  deriving stock (Eq, Enum)
 
 
-getTag :: TaggedIndex -> IndexType
-getTag = toEnum . getIndex . (`shiftR` 61)
 
-untagValue :: TaggedIndex -> Int
-untagValue (TaggedIndex i) = clearBit (clearBit i 62) 61
