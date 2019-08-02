9477c5e8ea44583455b0f910aad6bcc3701bb45f
Boarders <callan.mcgill@gmail.com>
2019-08-02 09:49:18 -0400

    Have interchange return both graphs

--- lib/pcg-graph/src/Data/Graph/Moves.hs
+++ lib/pcg-graph/src/Data/Graph/Moves.hs
@@ -11,13 +11,14 @@ import Data.Graph.NodeContext
 import qualified Data.Vector as V
 import qualified Data.Vector.Mutable as MV
 
+
 interchangeSubGraphs
   :: (HasHashValue c Int)
    => Graph f c e n (Graph f c e n t)
   -> Graph f c e n (Graph f c e n t)
   -> Int
   -> Int
-  -> Graph f c e n (Graph f c e n t)
+  -> (Graph f c e n (Graph f c e n t), Graph f c e n (Graph f c e n t))
 interchangeSubGraphs graph1 graph2 leafInd1 leafInd2 =
   let
     subGraph1 = graph1 ^. _leafReferences . (singular (ix leafInd1)) . _nodeData
@@ -27,8 +28,14 @@ interchangeSubGraphs graph1 graph2 leafInd1 leafInd2 =
   in
     if hashSubGraph1 == hashSubGraph2
       then
-        graph1 & _leafReferences
-               %~  V.modify
-                     \v -> MV.modify v (& _nodeData .~ subGraph2) leafInd1
+        let
+          newGraph1 = graph1 & _leafReferences
+                             %~  V.modify
+                                   \v -> MV.modify v (& _nodeData .~ subGraph2) leafInd1
+          newGraph2 = graph2 & _leafReferences
+                             %~  V.modify
+                                   \v -> MV.modify v (& _nodeData .~ subGraph1) leafInd2
+        in
+          (newGraph1, newGraph2)
       else
-        graph1
+        (graph1, graph2)
