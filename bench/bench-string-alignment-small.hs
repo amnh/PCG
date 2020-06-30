5fe5a3f9366dfac76110b05388b2a34057eb8dce
Alex Washburn <github@recursion.ninja>
2020-06-30 10:51:05 -0400

    Cleaning up build warnings

--- bench/bench-string-alignment-small.hs
+++ bench/bench-string-alignment-small.hs
@@ -12,10 +12,10 @@ import Data.MonoTraversable
 
 
 main :: IO ()
-main = let p = (128, 144)
-       in  benchStringAlignment $
-             \(m, x, y) -> ("two-hex-pref-gap" `isInfixOf` otoList (m ^. characterName))
+main = benchStringAlignment $
+            \(m, _, _) -> ("two-hex-pref-gap" `isInfixOf` otoList (m ^. characterName))
 {-
+       let p = (128, 144)
        in  benchStringAlignment $
              \(m, x, y) -> ((olength x == fst p && olength y == snd p)
                         ||  (olength x == snd p && olength y == fst p))
